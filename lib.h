372ce4d970d24ca3b2f03925c83fe536c01186bb
michele <michele.castellana@gmail.com>
2021-09-04 20:18:03 +0200

    Included the case where there is no need to make multiple plots

--- lib.h
+++ lib.h
@@ -1684,12 +1684,17 @@ void Plot::show(string prefix){
 
 	plot_command << "plot [" << t_p.value << " + epsilon:2.*pi] xe(K*Lambda(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")), ye(K*Phi(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")) smo csp lt " << i+1 << " noti \\\n";
 
+      }else{
+	//in this case, the circle of equal altitude is not cut through the meridian lambda = M_PI, and I make a single plot
+
+	plot_command << "plot [0.:2.*pi] xe(K*Lambda(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")), ye(K*Phi(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")) smo csp lt " << i+1 << " ti \"" << (sight_list[i]).body.name << " " << (sight_list[i]).time.to_string(display_precision).str().c_str() << " TAI\"\\\n";
+
+
       }
       
     }else{
       //in this case (sight_list[i]).GP.lambda.value is a monotonically increasing function of t: I find the value of t such that (sight_list[i]).GP.lambda.value = \pi and split the gnuplot plot command in two commands so as to avoid the horizontal line
       
-      plot_command << "plot [0.:2.*pi] xe(K*Lambda(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")), ye(K*Phi(t, " << (sight_list[i]).GP.phi.value << ", " << (sight_list[i]).GP.lambda.value << ", " << M_PI/2.0 - ((sight_list[i]).H_o.value) << ")) smo csp lt " << i+1 << " ti \"" << (sight_list[i]).body.name << " " << (sight_list[i]).time.to_string(display_precision).str().c_str() << " TAI\"\\\n";
 
     }
     

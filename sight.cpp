207466df92ec784efb3a377f5cebd547e4e6ec9c
michele <michele.castellana@gmail.com>
2021-09-06 20:31:53 +0200

    Removed comment

--- sight.cpp
+++ sight.cpp
@@ -44,7 +44,6 @@
 - make sure that running code cannot enter into infinite loop if weird values are entered from keyboard
 - include keyboard escape sequence to terminate code at any time
 - if phi_span < 1 degree, add one label on the y axis where you show both degrees and arcminutes
-- when reading Date from file, check that M variable lies in the correct interval according to the number of days of february
 - replace string with String everywhere
 - all functions ..read_from_file shouuld return a bool to signal whether the reading was successful or not
 - check whether H_o > 0, and if it is not, restart the procedure to enter the sight

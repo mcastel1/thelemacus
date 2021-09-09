fbacc44ea8b7e0ae0c41ce693be57b22921aae82
michele <michele.castellana@gmail.com>
2021-09-09 15:46:07 +0200

    added comment

--- sight.cpp
+++ sight.cpp
@@ -48,6 +48,7 @@
 - all functions ..read_from_file shouuld return a bool to signal whether the reading was successful or not
 - check whether H_o > 0, and if it is not, restart the procedure to enter the sight
 - adjust the number of points of coastlines as a function of the plot scale: the larger the scale, the lesser points
+- replace d and m in Angle::to_string with real degree symbol and ' symbol
 */
 
 using namespace std;

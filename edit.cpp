81bf8c0e17a193cf72419982d3fa4cc01775a0b5
michele <michele.castellana@gmail.com>
2021-11-30 15:35:25 +0100

    Added vectorial structure

--- edit.cpp
+++ edit.cpp
@@ -47,7 +47,12 @@ using namespace std;
 
 #include "lib.h"
 
+class position{
 
+public:
+  double lat, lon;
+
+};
 
 
 
@@ -55,34 +60,57 @@ int main(int argc, char *argv[]){
 
 
   ifstream infile;
+  ofstream outfile;
   string line;
-  double lon, lat;
+  int floor_old, floor_new;
+  vector< vector<position> > p(360);
   stringstream line_ins;
+  position t;
+  int i;
 
 
   infile.open("/Users/mcastellana/Documents/navigational_astronomy_large_files/coastlines_2/map_conv_sorted_by_latitude.csv");
+  outfile.open("/Users/mcastellana/Documents/navigational_astronomy_large_files/coastlines_2/map_conv_block.csv");
 
 
+  for(i=0; i<(int)(p.size()); i++){(p[i]).resize(0);}
+
+  
   line.clear();
   line_ins.clear();
   getline(infile, line);
 
+  floor_old = -1;
+
   while(!infile.eof()){
 
     line_ins << line;
-    line_ins >> lon >> lat;
+    line_ins >> (t.lon) >> (t.lat);
 
-    cout << lon << "\t" << lat << "\n";
+    cout << t.lon << "\t" << t.lat << "\n";
+
+    floor_new = floor(t.lat);
+
+    (p[floor_new]).push_back(t);
+
+    if(floor_new != floor_old){
+      for(i=0; i<(int)(p.size()); i++){(p[i]).clear();}
+      outfile << "\n\n";
+    }
     
     line.clear();
     line_ins.clear();
-    getline(infile, line); 
+    getline(infile, line);
+
+    floor_old = floor_new;
 
   }
 
 
   
   infile.close();
+  p.clear();
+  outfile.close();
 
   cout << "\n";
   return(0);

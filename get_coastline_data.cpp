f640b40efec0642c94555256223d754ca1734978
michele <michele.castellana@gmail.com>
2021-12-06 23:55:25 +0100

    Revised get_coastline_data.cpp: now the code takes as input the number N of points that will result in file path_file_selected_coastline_data . This allows to do without the scaling function g(x) in plot_dummy.plt

--- get_coastline_data.cpp
+++ get_coastline_data.cpp
@@ -26,8 +26,8 @@
   clear; clear; g++ get_coastline_data.cpp -llapack  -lgsl -lcblas -lm -O3 -Wno-deprecated -I/usr/local/include/gsl/ -I ./ -o get_coastline_data.o -Wall -DHAVE_INLINE -g
 
   ./get_coastline_data.o 
-  valgrind ./get_coastline_data.o   -p 45 -P 50 -l 1 -L 4 -e 10
-  valgrind --leak-check=full ./get_coastline_data.o    -p 45 -P 50 -l 1 -L 4 -e 10
+  valgrind ./get_coastline_data.o   -p 45 -P 50 -l 1 -L 4 -N 10
+  valgrind --leak-check=full ./get_coastline_data.o    -p 45 -P 50 -l 1 -L 4 -N 10
 
 
 */
@@ -52,7 +52,7 @@ using namespace std;
 
 #include <lib.h>
 
-//this code efficiently reads coastline data stored in path_file_coastline_data_blocked from latitudes p to P and longitudes l to L, and writes this data into path_file_selected_coastline_data, writing only one every 'every' points 
+//this code efficiently reads coastline data stored in path_file_coastline_data_blocked from latitudes p to P and longitudes l to L, and writes this data into path_file_selected_coastline_data, writing N_points points max 
 int main(int argc, char *argv[]){
 
   File file_n_line, file_coastline_data_blocked, outfile_selected_coastline_data;
@@ -62,12 +62,12 @@ int main(int argc, char *argv[]){
   int i, j, i_min = 0, i_max = 0, j_min = 0, j_max = 0, j_normalized = 0;
   //n_line[k] is the char count to be inserted in seekg to access directly to line k of file output, without going through all the lines in the file
   vector<unsigned int> n_line(360*(floor_max_lat-floor_min_lat+1));
-  unsigned int l, every = 0;
+  unsigned int l, N_points = 0, n = 0, every = 0;
   char* buffer = NULL;
   size_t pos_beg, pos_end;
 
 
-  while ((options = getopt(argc, argv, ":l:L:p:P:e:")) != -1) {
+  while ((options = getopt(argc, argv, ":l:L:p:P:N:")) != -1) {
 		
     switch (options) {
 
@@ -87,8 +87,8 @@ int main(int argc, char *argv[]){
       j_max = atoi(optarg);
       break;	
 
-   case 'e':
-     every = (unsigned int)atoi(optarg);
+   case 'N':
+     N_points = (unsigned int)atoi(optarg);
       break;	
  
       
@@ -168,14 +168,18 @@ int main(int argc, char *argv[]){
   outfile_selected_coastline_data.remove(String(""));  
   outfile_selected_coastline_data.open(String("out"), String(""));
 
+  //count how many datapoints are in data
+  n = count(data.begin(), data.end(), ',');
+ 
+  every = (unsigned int)(((double)n)/((double)N_points));
+
   i=0;
   pos_beg = 0;
   pos_end = data.find(" ", pos_beg);
   while(pos_end != (string::npos)){
 
-    //I write points in data to outfile_selected_coastline_data only every e points
+    //I write points in data to outfile_selected_coastline_data in such a way to write N_points points to the most
     if((i % every) == 0){
-
     
       line.clear();
       line = data.substr(pos_beg, pos_end - pos_beg + 1).c_str();

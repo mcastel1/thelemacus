001071eda3f0ee189d1efc460fa2e24dde6e36e5
michele <michele.castellana@gmail.com>
2022-01-12 18:40:58 +0100

    Added note

--- sight.cpp
+++ sight.cpp
@@ -69,6 +69,7 @@
 - finisci di programmare Sight::modify (mancano le modifiche ad alcuni item)
 - solve issue that sed crashes if replaced string is too long
 - check check_double function for sign of double
+- type of related_sight should be int -> Int
 */
 
 

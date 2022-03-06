8b70a0806b2c78323310237abc902e54609994fe
Michele <michele.castellana@gmail.com>
2022-03-06 23:25:07 +0100

    Added note

--- sight_gui.cpp
+++ sight_gui.cpp
@@ -61,7 +61,8 @@ using namespace std;
  - when you click on modify and then press reduce, the code should not insert a new item in listcontrol, but replace the old one
 - if you modify a field in SightFrame, do not kill the focus on the field and press Reduce, the content of the field is not written in the non-GUI object, while it should be -> fix it. 
 - add recent items to dropdown menus
- - add instrumental error 
+ - add instrumental error
+ - add condition that an error message is prompted only if the GUI field is enabled not only to CheckLength, but also to all other Check* classes
  */
 
 

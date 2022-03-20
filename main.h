343664ad6aeba1cd95c463ffffb3405ca7e0b6ac
Michele <michele.castellana@gmail.com>
2022-03-20 16:20:22 +0100

    Moved all :: stuff from lib.h to main.h

--- main.h
+++ main.h
@@ -1,12 +1,109 @@
-//
-//  main.h
-//  sight_release
-//
-//  Created by MacBook Pro on 20/03/2022.
-//
-
-#ifndef main_h
-#define main_h
+#include "wx/wxprec.h"
+#include "wx/checkbox.h"
+#include "wx/combobox.h"
+#include "wx/dc.h"
+#include "wx/display.h"
+#include "wx/listctrl.h"
+#include "wx/slider.h"
 
 
-#endif /* main_h */
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "chartdir.h"
+
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <list>
+#include <stdio.h>
+
+
+#include "gsl_rng.h"
+// #include "gsl_randist.h"
+// #include "gsl_vector.h"
+// #include "gsl_matrix.h"
+// #include "gsl_eigen.h"
+#include "gsl_blas.h"
+#include "gsl_sf_pow_int.h"
+#include "gsl_sf_exp.h"
+#include "gsl_errno.h"
+#include "gsl_math.h"
+#include "gsl_spline.h"
+#include "gsl_integration.h"
+#include "gsl_roots.h"
+// #include "gsl_complex.h"
+// #include "gsl_complex_math.h"
+
+
+
+using namespace std;
+
+class BodyField;
+class LimbField;
+template<class T> class CheckField;
+template<class P> class AngleField;
+class LengthField;
+class DateField;
+class ChronoField;
+template<class P> class StringField;
+class MyApp;
+class MessageFrame;
+template<class F_YES, class F_NO> class QuestionFrame;
+class ListFrame;
+class SightFrame;
+class ChartFrame;
+class PlotFrame;
+class ChartPanel;
+
+struct CheckBody;
+struct CheckLimb;
+template<class T> struct CheckCheck;
+struct CheckChrono;
+template<class P> class CheckAngle;
+template<class P> struct CheckSign;
+template<class P> struct CheckArcDegree;
+template<class P> struct CheckArcMinute;
+struct CheckLength;
+class CheckDate;
+class CheckYear;
+class CheckMonth;
+class CheckDay;
+struct CheckHour;
+struct CheckMinute;
+struct CheckSecond;
+template<class P> struct CheckString;
+template<class P> struct SetStringToCurrentTime;
+struct TabulateDays;
+template<class T> class PrintErrorMessage;
+struct OnSelectInListBox;
+
+class Catalog;
+class Limb;
+class Angle;
+class Length;
+class String;
+class Plot;
+class File;
+class Time;
+class Date;
+class Chrono;
+class Route;
+class Sight;
+class Atmosphere;
+class Answer;
+class Body;
+
+class MyApp: public wxApp{
+public:
+    virtual bool OnInit();
+};
+

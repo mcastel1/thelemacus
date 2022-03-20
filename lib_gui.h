d4093f360f4078a7259069885d2c1f633979efac
Michele <michele.castellana@gmail.com>
2022-03-20 18:50:21 +0100

    Commented out useless includes

--- lib_gui.h
+++ lib_gui.h
@@ -7,23 +7,6 @@
 
 
 
-BEGIN_EVENT_TABLE(DrawPanel, wxPanel)
-// some useful events
-/*
- EVT_MOTION(DrawPanel::mouseMoved)
- EVT_LEFT_DOWN(DrawPanel::mouseDown)
- EVT_LEFT_UP(DrawPanel::mouseReleased)
- EVT_RIGHT_DOWN(DrawPanel::rightClick)
- EVT_LEAVE_WINDOW(DrawPanel::mouseLeftWindow)
- EVT_KEY_DOWN(DrawPanel::keyPressed)
- EVT_KEY_UP(DrawPanel::keyReleased)
- EVT_MOUSEWHEEL(DrawPanel::mouseWheelMoved)
- */
-
-// catch paint events
-EVT_PAINT(DrawPanel::PaintEvent)
-
-END_EVENT_TABLE()
 
 
 

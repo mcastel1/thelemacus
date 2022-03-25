4f2871f5bf6fd19b3da1e518a83b8f1c2eaff159
Michele <michele.castellana@gmail.com>
2022-03-25 18:43:22 +0100

    Added comment

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@
  - make sure that LengthField allows to enter lengths in both meters and nm
  - change the initialization of structs (such as     (check.p) = this;) by transforming the structs into classes, creating their constructor, transforming check into a pointer and replacing     (check.p) = this; with check = new Check(this); do this for all objects
  - set log scale on slider
+ - fix issue that related sight is not properly deleted when a route is deleted
  */
 
 

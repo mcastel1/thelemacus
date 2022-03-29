79a0f04a50c23a6c1802466fc475761647e46442
Michele <michele.castellana@gmail.com>
2022-03-29 16:25:48 +0200

    Created value_slider_now

--- lib.h
+++ lib.h
@@ -1760,7 +1760,7 @@ public:
     wxSlider* slider;
     PrintErrorMessage<ChartFrame>* print_error_message;
     //this variable is true if the user has started drawing a selection rectangle on image, by right-clicking on image and thus forming one of the corners of the rectangle, and zero otherwise.
-    unsigned int /*this stores the value of slider*/value_slider_old;
+    unsigned int /*this stores the old value of slider and the current value of the slider*/value_slider_old, value_slider_now;
     vector<double> x, y;
     //idling = true means that the user is interacting with a temporary dialog window, thus all the handlers of wxFOCUS_EVENT do not make sense when idling = true and they will be disabled until idling is set back to false
     bool idling;

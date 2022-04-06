fd0728012bb3a1ef72a5a7b7c32c767a95bfe3ef
Michele <michele.castellana@gmail.com>
2022-04-06 19:22:24 +0200

    Cleaned up

--- lib.cpp
+++ lib.cpp
@@ -6422,7 +6422,7 @@ void DrawPanel::Render(wxDC&  dc){
     wxString wx_string;
     //this = true if, while drawing the x or y axis labels, the label that I one is about to draw is the first one
     bool first_label;
-    unsigned int i, j, l, start, end;
+    unsigned int i, j, start, end;
     
     wxBrush brush(wxColour(/*the first three entries are the rgb code for the color*/255, 0, 0, /*the last is the degree of transparency of the color*/25));
     //    brush.SetStyle(wxBRUSHSTYLE_TRANSPARENT);

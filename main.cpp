1288553f1530320661196dc68a86b168d304aa96
Michele <michele.castellana@gmail.com>
2022-04-12 14:47:10 +0200

    Deleted notes.

--- main.cpp
+++ main.cpp
@@ -12,21 +12,16 @@
 /*
  notes: set parents to constructors of all frames that you defined
  - when you click on modify and then press reduce, the code should not insert a new item in listcontrol, but replace the old one
- - add recent items to dropdown menus
  - add instrumental error
  - add condition that an error message is prompted only if the GUI field is enabled not only to CheckLength, but also to all other Check* classes
- - add error message if phi_min, phi_max etc... exceed the min and max latitudes in the data file.
  - all quantities in pixels must be relative to screen size, not absolute pixel size.
- - re-arrange class definitions properly
  - add lambert projection and 3D sphere with no projection
  - make sure you can recover a correct chart after the print error message has been prompted because the zoom factor is too large
- - make sure that LengthField allows to enter lengths in both meters and nm
  - change the initialization of structs (such as     (check.p) = this;) by transforming the structs into classes, creating their constructor, transforming check into a pointer and replacing     (check.p) = this; with check = new Check(this); do this for all objects
  - fix issue that related sight is not properly deleted when a route is deleted
  -     Now the slider in log scale works, but there is a problem when i de-zoom back to 1:1
  - set up a proper output to a log file
  - turn gamma_lambda into an unsigned int
- - add a dropdown menu with the units of measure in LengthField
  - add margins in chart in such a way that ylabels are not cut
  - the number of points with which routes are plotted should be adapted dynamically to the zooming factor
  - fix 'Error: not all characters could be read'

4d2e1916dc80e5f00dd77109883faf52ef34d55a
Michele <michele.castellana@gmail.com>
2022-04-20 10:22:37 +0200

    Pasted corrected expressions for x and y in the 3D projection -> revise syntax to adapt them to C++.

--- lib.cpp
+++ lib.cpp
@@ -8080,10 +8080,10 @@ void DrawPanel::ScreenToMercator(wxPoint p, double* x, double* y){
 //converts the geographic Position p  to the  3D projection (x,y)
 void DrawPanel::GeoTo3D(Position p, double* x, double* y){
     
-    (*x) = ( ((d.value)*(cos((euler_c))*cos((euler_a) - (p.lambda))*cos(((p.phi))) + sin((euler_c))*(-(cos((euler_b))*cos(((p.phi)))*sin((euler_a) - ((p.lambda)))) + sin((euler_b))*sin(((p.phi))))))/
-            ((d.value) + ((l).value) + cos((euler_a) - ((p.lambda)))*cos(((p.phi)))*sin((euler_c)) + cos((euler_b))*cos((euler_c))*cos(((p.phi)))*sin((euler_a) - ((p.lambda))) - cos((euler_c))*sin((euler_b))*sin(((p.phi)))) );
+    (*x) = (d*(Cos(c)*Cos(a - \[Lambda])*Cos(\[Phi]) - Sin(c)*(Cos(b)*Cos(\[Phi])*Sin(a - \[Lambda]) + Sin(b)*Sin(\[Phi]))))/
+    (d + l + Cos(a - \[Lambda])*Cos(\[Phi])*Sin(c) + Cos(b)*Cos(c)*Cos(\[Phi])*Sin(a - \[Lambda]) + Cos(c)*Sin(b)*Sin(\[Phi]));
     
-    (*y) = ( ((d.value)*(cos(((p.phi)))*sin((euler_b))*sin((euler_a) - ((p.lambda))) + cos((euler_b))*sin(((p.phi)))))/((d.value) + ((l).value) + cos((euler_a) - ((p.lambda)))*cos(((p.phi)))*sin((euler_c)) + cos((euler_b))*cos((euler_c))*cos(((p.phi)))*sin((euler_a) - ((p.lambda))) - cos((euler_c))*sin((euler_b))*sin(((p.phi)))) );
+    (*y) = (-(d*Cos(\[Phi])*Sin(b)*Sin(a - \[Lambda])) + d*Cos(b)*Sin(\[Phi]))/(d + l + Cos(a - \[Lambda])*Cos(\[Phi])*Sin(c) + Cos(b)*Cos(c)*Cos(\[Phi])*Sin(a - \[Lambda]) + Cos(c)*Sin(b)*Sin(\[Phi]));
     
     
     

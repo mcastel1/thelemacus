d4485c82d71e8a075e16301fd800ca721be98015
Michele <michele.castellana@gmail.com>
2022-05-07 16:16:45 +0200

    Added note.

--- main.cpp
+++ main.cpp
@@ -29,7 +29,8 @@
  - in the 3d projection two lengths l and d are redundant: remove one
  -  I cannot find a way to invert the equations to obtain lambda and phi from x, y in the 3D projection
  - fill the cases in Route::draw_3D for l and o
- - reset button does not bring chart to original configuration 
+ - reset button does not bring chart to original configuration
+ - check why body field gets empty after you entered body name and kill focus
  */
 
 

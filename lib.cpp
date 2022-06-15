2a80e59723c16e2270ccd02ad474a2958ca02d07
Michele <michele.castellana@gmail.com>
2022-06-15 16:07:17 +0200

    I use the old draw function to draw minor ticks on the lambda axis

--- lib.cpp
+++ lib.cpp
@@ -8514,7 +8514,7 @@ void DrawPanel::Draw_3D(void){
                 (((route.reference_position).lambda).value) - ((lambda_saved.value)+M_PI/2.0) < delta_lambda;
                 (((route.reference_position).lambda).value) += delta_lambda_minor){
                     
-                    route.draw_3D((parent->n_points_minor_ticks).value, 0x0000ff, -1, this, String(""));
+                    route.draw((parent->n_points_minor_ticks).value, 0x0000ff, -1, this);
                     
                 }
                 

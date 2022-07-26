1cf3aa72fd3261cbd0d5270c9f05bb67ded8b52e
Michele <michele.castellana@gmail.com>
2022-07-26 15:37:02 +0200

    Fixed missing setting of zoom_factor.

--- lib.cpp
+++ lib.cpp
@@ -9254,8 +9254,9 @@ void DrawPanel::Draw_3D(void){
     label_phi.resize(0);
     
     
-    //set d, the boundaries of x and y for the chart, and the latitudes and longitudes which comrpise circle_observer
+    //set d, zoom_factor, the boundaries of x and y for the chart, and the latitudes and longitudes which comrpise circle_observer
     d.set(String(""), -1.0 + sqrt(1.0 + gsl_pow_2(tan(circle_observer.omega))), String(""));
+    (parent->zoom_factor).set(String(""), (d_0.value)/(d.value), String(""));
     (this->*Set_x_y_min_max)();
     (this->*Set_lambda_phi_min_max)();
 

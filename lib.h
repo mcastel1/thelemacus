05d1b0dccead34ea2865180dfa2f56db969a53a1
Michele <michele.castellana@gmail.com>
2022-09-06 23:06:46 +0200

    Fixed bug wxSizer -> wxBoxSizer in ListFrame

--- lib.h
+++ lib.h
@@ -1866,7 +1866,8 @@ public:
     RouteFrame* route_frame;
     wxButton *button_add_sight, *button_delete_sight, *button_add_position, *button_delete_position, *button_add_route, *button_delete_route;
     wxBitmapButton *button_modify_sight, *button_transport_sight, *button_transport_position, *button_modify_position, *button_modify_route;
-    wxSizer* sizer_h, *sizer_v, *sizer_buttons_sight, *sizer_buttons_position, *sizer_buttons_route;
+    wxBoxSizer *sizer_h, *sizer_v;
+    wxSizer *sizer_buttons_sight, *sizer_buttons_position, *sizer_buttons_route;
     wxStaticBoxSizer* sizer_box_sight, *sizer_box_position, *sizer_box_route;
     DeleteSight *delete_sight, *delete_sight_and_related_route;
     DeleteRoute *delete_route, *delete_route_and_related_sight;

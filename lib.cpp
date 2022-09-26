6cb585780f62e13038081fee2785eb9b2351fce0
Michele <michele.castellana@gmail.com>
2022-09-26 15:36:21 +0200

    Cleared superfluous settings of background color

--- lib.cpp
+++ lib.cpp
@@ -10910,7 +10910,7 @@ void DrawPanel::OnMouseMovement(wxMouseEvent &event){
         s.str("");
         s << (((parent->parent)->p_now).phi).to_string(String("NS"), (display_precision.value), true) << " " << (((parent->parent)->p_now).lambda).to_string(String("EW"), (display_precision.value), true);
         text_position_end->SetLabel(wxString(s.str().c_str()));
-        text_position_end->SetBackgroundColour(wxGetApp().background_color);
+//        text_position_end->SetBackgroundColour(wxGetApp().background_color);
         text_position_end->SetPosition(wxPoint((position_screen_now.x)-(position_draw_panel.x), (position_screen_now.y)-(position_draw_panel.y)));
         PaintNow();
     }
@@ -11276,7 +11276,7 @@ void DrawPanel::OnMouseRightDown(wxMouseEvent &event){
         s.clear();
         s << (((parent->parent)->p_start).phi).to_string(String("NS"), (display_precision.value), true) << " " << (((parent->parent)->p_start).lambda).to_string(String("EW"), (display_precision.value), true);
         text_position_start->SetLabel(wxString(s.str().c_str()));
-        text_position_start->SetBackgroundColour(wxGetApp().background_color);
+//        text_position_start->SetBackgroundColour(wxGetApp().background_color);
         text_position_start->SetPosition(wxPoint((position_start_selection.x)-(position_draw_panel.x), (position_start_selection.y)-(position_draw_panel.y)));
         
         //        cout << "((parent->parent)->p_start) = {" << (((parent->parent)->p_start).lambda).to_string(String("EW"), (display_precision.value), false) << " , " << (((parent->parent)->p_start).phi).to_string(String("NS"), (display_precision.value), false) << " }\n";

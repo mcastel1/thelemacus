3df88df964c3e1bd86f834abf2fa39594a71417e
Michele <michele.castellana@gmail.com>
2022-10-05 08:08:52 +0200

    data_3d -> p_coastline

--- lib.h
+++ lib.h
@@ -1967,7 +1967,7 @@ public:
     int /*the # of the sight/route/position which is highlighted because the mouse is hovering over it in listcontrol_sights/routes/positions*/highlighted_sight, highlighted_route, highlighted_position;
     //data_x[i][j] is a vector which contains the (x-value of) the datapoints within the block at (shifted) latitude i and longitude j in file path_file_coastline_data_blocked
     Double size_small_button_over_width_screen;
-    vector< vector< vector<Position> > > data_3d;
+    vector< vector< vector<Position> > > p_coastline;
     Position /*these are the positions where the right mouse button is clicked at the beginning, current time and at the end of the drawing process for the selection rectangle on the world's chart*/p_start, p_now, p_end;
     vector<Color> color_list;
     //the color with which an item is highlighted in ListFrame, when mouse hovers over the related route or position in the plot area

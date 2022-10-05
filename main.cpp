b99efb6a72b3472f9b494348136232c69d26b818
Michele <michele.castellana@gmail.com>
2022-10-05 08:39:39 +0200

    It works

--- main.cpp
+++ main.cpp
@@ -46,7 +46,6 @@
  - if displayprecision is too small, contiguous labels on paralles / merdisians show the same value
  - when you press button_reduce in SightFrame, you should call on_change_selection_in_listcontrol_sights in such a way that some buttons are deactivated in listcontrol_sights, and same for other listcontrols
  -  in the 3d projection, plot the number of points in each lat/long square according to its inclination with respect to the observer
- - convert data_x directly to DrawPanel coordinates also for the mercator projection 
  */
 
 

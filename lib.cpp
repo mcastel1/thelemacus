e13ba150ed7e262d3732b92f143a68233f3aba2c
Michele <michele.castellana@gmail.com>
2022-10-13 16:33:22 +0200

    Changed Fore - Back ground color

--- lib.cpp
+++ lib.cpp
@@ -8751,7 +8751,7 @@ void DrawPanel::PutLabel(const Position& q, Angle min, Angle max, String mode){
         ((*labels).back())->SetPosition(p);
         
         //the constructor of StaticText sets by default the background color to wxGetApp().frame_background_color, becasue most static texts are displayed on top of frames, but here the static text is displayed on top of a wxImage, so I set the appropriate background color
-        ((*labels).back())->SetBackgroundColour(wxGetApp().background_color);
+        ((*labels).back())->SetForegroundColour(wxGetApp().foreground_color);
         
         first_label = false;
         
@@ -10357,7 +10357,7 @@ template<class T>void CheckBody::operator()(T& event){
         }
         i--;
         
-        if(check || ((((p->name)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->name)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->name)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->name)->GetValue()).ToStdString())) == String("")))){
             //p->check either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             if(check){
@@ -10409,7 +10409,7 @@ template<class T>void CheckBody::operator()(T& event){
             //if check is true (false) -> set ok to true (false)
             (p->ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in name
-            (p->name)->SetBackgroundColour(wxGetApp().background_color);
+            (p->name)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -10446,13 +10446,13 @@ template<class T> void CheckLimb::operator()(T &event){
         check = ((s == String("upper")) || (s == String("lower")) || (s == String("center")));
         
         
-        if(check || ((((p->name)->GetBackgroundColour()) != (wxGetApp().error_color)) && (s == String("")))){
+        if(check || ((((p->name)->GetForegroundColour()) != (wxGetApp().error_color)) && (s == String("")))){
             //p->name either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set ok to true (false)
             (p->ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in name
-            (p->name)->SetBackgroundColour(wxGetApp().background_color);
+            (p->name)->SetForegroundColour(wxGetApp().foreground_color);
             
             
         }else{
@@ -10524,13 +10524,13 @@ template<class P> template <class T> void CheckSign<P>::operator()(T &event){
         }
         
         
-        if(check || ((((p->sign)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->sign)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->sign)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->sign)->GetValue()).ToStdString())) == String("")))){
             //p->sign either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set sign_ok to true (false)
             (p->sign_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in sign
-            (p->sign)->SetBackgroundColour(wxGetApp().background_color);
+            (p->sign)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -12382,12 +12382,12 @@ template<class P> template<class T> void CheckArcDegree<P>::operator()(T &event)
         
         check = check_unsigned_int(((p->deg)->GetValue()).ToStdString(), NULL, true, 0, 360);
         
-        if(check || ((((p->deg)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->deg)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->deg)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->deg)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set deg_ok to true (false)
             (p->deg_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in deg
-            (p->deg)->SetBackgroundColour(wxGetApp().background_color);
+            (p->deg)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -12416,13 +12416,13 @@ template<class P> template <class T> void CheckArcMinute<P>::operator()(T &event
         
         check = check_double(((p->min)->GetValue()).ToStdString(), NULL, true, 0.0, 60.0);
         
-        if(check || ((((p->min)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->min)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->min)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->min)->GetValue()).ToStdString())) == String("")))){
             //p->min either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set min_ok to true (false)
             (p->min_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in min
-            (p->min)->SetBackgroundColour(wxGetApp().background_color);
+            (p->min)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -12453,13 +12453,13 @@ template<class P> template <class T> void CheckLengthValue<P>::operator()(T &eve
         
         check = check_double(((p->value)->GetValue()).ToStdString(), NULL, true, 0.0, DBL_MAX);
         
-        if(check || ((((p->value)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->value)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->value)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->value)->GetValue()).ToStdString())) == String("")))){
             //p->value either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set value_ok to true (false)
             (p->value_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in value
-            (p->value)->SetBackgroundColour(wxGetApp().background_color);
+            (p->value)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -12496,12 +12496,12 @@ template<class P> template <class T> void CheckLengthUnit<P>::operator()(T &even
         }
         i--;
         
-        if(check || ((((p->unit)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->unit)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->unit)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->unit)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set unit_ok to true (false)
             (p->unit_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in deg
-            (p->unit)->SetBackgroundColour(wxGetApp().background_color);
+            (p->unit)->SetForegroundColour(wxGetApp().foreground_color);
             
             
         }else{
@@ -14827,13 +14827,13 @@ template<class T> void CheckYear::operator()(T&event){
         
         check = check_unsigned_int(((p->year)->GetValue()).ToStdString(), NULL, false, 0, 0);
         
-        if(check || ((((p->year)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->year)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->year)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->year)->GetValue()).ToStdString())) == String("")))){
             //p->year either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set year_ok to true (false)
             (p->year_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in year
-            (p->year)->SetBackgroundColour(wxGetApp().background_color);
+            (p->year)->SetForegroundColour(wxGetApp().foreground_color);
             
             if(check && (p->month_ok)){
                 
@@ -14872,13 +14872,13 @@ template<class T> void CheckMonth::operator()(T&event){
         
         check = check_unsigned_int(((p->month)->GetValue()).ToStdString(), NULL, true, 1, 12+1);
         
-        if(check || ((((p->month)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->month)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->month)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->month)->GetValue()).ToStdString())) == String("")))){
             //p->month either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
             
             //if check is true (false) -> set month_ok to true (false)
             (p->month_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in month
-            (p->month)->SetBackgroundColour(wxGetApp().background_color);
+            (p->month)->SetForegroundColour(wxGetApp().foreground_color);
             
             if(check && (p->year_ok)){
                 
@@ -14930,12 +14930,12 @@ template<class T> void CheckDay::operator()(T& event){
         }
         
         
-        if(check || ((((p->day)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->day)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->day)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->day)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set day_ok to true (false)
             (p->day_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in day
-            (p->day)->SetBackgroundColour(wxGetApp().background_color);
+            (p->day)->SetForegroundColour(wxGetApp().foreground_color);
             
             
         }else{
@@ -15074,12 +15074,12 @@ template<class T> void CheckHour::operator()(T &event){
         check = check_unsigned_int(((p->hour)->GetValue()).ToStdString(), NULL, true, 0, 24);
         
         
-        if(check || ((((p->hour)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->hour)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->hour)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->hour)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set hour_ok to true (false)
             (p->hour_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in deg
-            (p->hour)->SetBackgroundColour(wxGetApp().background_color);
+            (p->hour)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -15114,12 +15114,12 @@ template<class T> void CheckMinute::operator()(T &event){
         
         check = check_unsigned_int(((p->minute)->GetValue()).ToStdString(), NULL, true, 0, 60);
         
-        if(check || ((((p->minute)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->minute)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->minute)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->minute)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set minute_ok to true (false)
             (p->minute_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in minute
-            (p->minute)->SetBackgroundColour(wxGetApp().background_color);
+            (p->minute)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -15154,12 +15154,12 @@ template<class T> void CheckSecond::operator()(T &event){
         
         check = check_double(((p->second)->GetValue()).ToStdString(), NULL, true, 0.0, 60.0);
         
-        if(check || ((((p->second)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->second)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->second)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->second)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set second_ok to true (false)
             (p->second_ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in deg
-            (p->second)->SetBackgroundColour(wxGetApp().background_color);
+            (p->second)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             
@@ -15253,12 +15253,12 @@ template<class T>void CheckRouteType::operator()(T& event){
         }
         
         
-        if(check || ((((p->name)->GetBackgroundColour()) != (wxGetApp().error_color)) && (String((((p->name)->GetValue()).ToStdString())) == String("")))){
+        if(check || ((((p->name)->GetForegroundColour()) != (wxGetApp().error_color)) && (String((((p->name)->GetValue()).ToStdString())) == String("")))){
             
             //if check is true (false) -> set ok to true (false)
             (p->ok) = check;
             //the background color is set to white, because in this case there is no erroneous value in name
-            (p->name)->SetBackgroundColour(wxGetApp().background_color);
+            (p->name)->SetForegroundColour(wxGetApp().foreground_color);
             
         }else{
             

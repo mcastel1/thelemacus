d0187995567dd29e25d81c110d00f8f45cb4d397
Michele <michele.castellana@gmail.com>
2022-10-21 12:39:47 +0200

    When a sight is reduced, buttons are disabled in listcontrol_sights

--- lib.cpp
+++ lib.cpp
@@ -15450,6 +15450,10 @@ void SightFrame::OnPressReduce(wxCommandEvent& event){
     (parent->listcontrol_sights)->set((parent->plot)->sight_list);
     (parent->listcontrol_routes)->set((parent->plot)->route_list);
     
+    //given that I have reset the content of listcontrol_sights and listcontrol_routes, no items will be selected in these ListControls -> I disable their disableable buttons
+    (parent->listcontrol_sights)->EnableButtons(false);
+    (parent->listcontrol_routes)->EnableButtons(false);
+
     parent->DrawAll();
     
     event.Skip(true);

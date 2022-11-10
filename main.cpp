7a8624b3c7b8c9d0ed71833656508b4dfcf35c23
Michele <michele.castellana@gmail.com>
2022-11-10 15:04:56 +0100

    Changed notes.

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@
 
  ********** THINGS TO FIX ************
  
- - transfrom all angular quantities in units of radians
  - make sure that Route::draw is used every time a Route is drawn (no useless copies of the same lines of code) -> use the fact that in the mercator projection loxodromes are straight lines to incoprporate them in the new Draw function
  - in Route::draw_3D, code the part for loxodrome curves
  - in DrawPanel::draw_3D : when drawing minor ticks on the x axis : because I am drawing a loxodrome, I am using the old function Route::draw -> replace this with Route::draw_3D in the future

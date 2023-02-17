eb5f50f54d776badfdc27db7c9e99927406c4acd
Michele <michele.castellana@gmail.com>
2023-02-17 12:24:26 +0100

    Included dynamical computation of size_label_horizontal

--- lib.cpp
+++ lib.cpp
@@ -9122,17 +9122,14 @@ void DrawPanel::Draw_Mercator(void){
     Position q, /*the geographic positions corresponding to the NW (SE) boundary of of the plot area, moved to the interior of the plot area by one pixel. These will be used to plot parallels and meridians in such a way that they don't hit the boundary of the plot area*/p_NW, p_SE;
     String prefix, new_prefix;
     wxPoint p;
+    wxString dummy_label;
     
     //append \t to prefix
     prefix = String("");
     new_prefix = prefix.append(String("\t"));
     
-    //take the angle 0° 0.0' expresed with display_precision: the height of this angle label is the largest possible -> set it equal to size_label_vertical
-    size_label_vertical = (GetTextExtent(wxString((Angle(0,  0.0).to_string(String("NS"), (display_precision.value), false)))).GetHeight());
-    //take the angle 333° 33.333333....' expresed with display_precision: the size of this angle label is the largest possible -> set it equal to size_label_horizontal
-    size_label_horizontal = (GetTextExtent(wxString((Angle(333, 100.0/3.0).to_string(String("EW"), (display_precision.value), false)))).GetWidth());
-    
-    
+      
+    //here I compute multiple quantities relative to the y axis: this computation is done here, at the very beginning of Draw_Mercator, because these quantitites will be needed immediatly to compute size_label_horizontal
     //set phi_start, phi_end and delta_phi
     phi_span =  (((parent->phi_max).normalize_pm_pi_ret()).value) - (((parent->phi_min).normalize_pm_pi_ret()).value);
     
@@ -9162,12 +9159,11 @@ void DrawPanel::Draw_Mercator(void){
     //set phi_start/end
     (phi_start.value) = floor((((parent->phi_min).normalize_pm_pi_ret()).value)/delta_phi)*delta_phi;
     (phi_end.value) = (((parent->phi_max).normalize_pm_pi_ret()).value);
- 
 
     
-    /*
-     //compute size of largest label on parallel
-    for(first_label = true,
+     //compute size of largest label on parallel: run through all labels on parallels and set size_label_horizontal as the size of the largest label on parallel that has ben found
+    for(size_label_horizontal = 0,
+        first_label = true,
         //set the label precision: if gamma_phi = 1, then labels correspond to integer degrees, and I set label_precision = display_precision. If not, I take the log delta_phi*K*60 (the spacing between labels in arcminuted) -> I obtain the number of digits reqired to proprely display arcminutes in the labels -> round it up for safety with ceil() -> add 2 -> obtain the number of digits to safely display the digits before the '.' (2) and the digits after the '.' in the arcminute part of labels
         (label_precision.value) = (gamma_phi == 1) ? (display_precision.value) : (2+ceil(fabs(log(delta_phi*K*60)))),
         ((q.phi).value) = (phi_start.value),
@@ -9176,11 +9172,17 @@ void DrawPanel::Draw_Mercator(void){
         ((q.phi).value) += delta_phi
         ){
         
-        DrawLabel(q, parent->phi_min, parent->phi_max, label_precision, String("NS"));
+        WriteLabel(q, parent->phi_min, parent->phi_max, label_precision, String("NS"), &dummy_label);
+        
+        if((GetTextExtent(dummy_label).GetWidth()) > size_label_horizontal){
+            size_label_horizontal = (GetTextExtent(dummy_label).GetWidth());
+        }
         
     }
-     */
-    
+     
+    //take the angle 0° 0.0' expresed with display_precision: the height of this angle label is the largest possible -> set it equal to size_label_vertical
+    size_label_vertical = (GetTextExtent(wxString((Angle(0,  0.0).to_string(String("NS"), (display_precision.value), false)))).GetHeight());
+ 
     
     //clears all labels previously drawn
     for(i=0; i<label_lambda.size(); i++){(label_lambda[i])->Destroy();}

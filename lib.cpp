749d1a60753599856dc8eb6b31ae0453244daa32
Michele <michele.castellana@gmail.com>
2023-03-09 11:15:31 +0100

    temp -> image_time_interval_not_ok

--- lib.cpp
+++ lib.cpp
@@ -15240,11 +15240,11 @@ void SightFrame::AllOk(void){
     text_time_interval_not_ok->SetLabel(wxString(time_interval_ok ? "" : "Time not enclosed in ephemerides' data!"));
     
     
-    wxImage temp = (wxBitmap(path_file_app_icon, wxBITMAP_TYPE_PNG).ConvertToImage());
-    RescaleProportionally(&temp, wxGetApp().size_small_button);
+    wxImage image_time_interval_not_ok = (wxBitmap(path_file_app_icon, wxBITMAP_TYPE_PNG).ConvertToImage());
+    RescaleProportionally(&image_time_interval_not_ok, wxGetApp().size_small_button);
 
     
-    image_time_interval_status->SetBitmap(time_interval_ok ? wxNullBitmap : wxBitmap(temp, wxBITMAP_SCREEN_DEPTH));
+    image_time_interval_status->SetBitmap(time_interval_ok ? wxNullBitmap : wxBitmap(image_time_interval_not_ok, wxBITMAP_SCREEN_DEPTH));
   
     
     

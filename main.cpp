5198cb1287f993040085d5a04c209a6e7d14cb66
Michele <michele.castellana@gmail.com>
2023-08-26 16:34:18 +0200

    Removed note

--- main.cpp
+++ main.cpp
@@ -41,7 +41,6 @@
  ********** THINGS TO FIX ************
  
  - make scaling of images to absolute sizes 20,20 relative to screen size
- - make sure that Route::draw is used every time a Route is drawn (no useless copies of the same lines of code) -> use the fact that in the mercator projection loxodromes are straight lines to incoprporate them in the new Draw function
  - Make sure that Draw is not called uselessly some times
  - in DrawPanel::draw_3D : when drawing minor ticks on the x axis : because I am drawing a loxodrome, I am using the old function Route::draw -> replace this with Route::draw_3D in the future
  - add check on zoom factor in OnMouseRightDown for the 3D projections

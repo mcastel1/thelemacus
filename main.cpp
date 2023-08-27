ebe5811be6c0bb2ecb8c7e84fb9c23dc3af0a42b
Michele <michele.castellana@gmail.com>
2023-08-27 13:30:12 +0200

    Added note

--- main.cpp
+++ main.cpp
@@ -40,6 +40,7 @@
  
  ********** THINGS TO FIX ************
  
+ -meridians disappear when you move E/W in mercator projection
  - Make sure that Draw is not called uselessly some times
  - in DrawPanel::draw_3D : when drawing minor ticks on the x axis : because I am drawing a loxodrome, I am using the old function Route::draw -> replace this with Route::draw_3D in the future
  - add check on zoom factor in OnMouseRightDown for the 3D projections

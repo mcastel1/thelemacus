0259eddd6710aab26b7dfd86ad03c52330f0d110
Michele <michele.castellana@gmail.com>
2023-09-07 08:08:36 +0200

    Added note

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@
  ********** THINGS TO ADD ************
  
  - when one double clicks on a sight/route/position in ListFrame, open the modify panel
+ - fill up selection 'rectangle' in 3D projection by using a bundle of curves at fixed lat or lon
  - change     highlight_color = color_selected_item and     highlight_font = default_font;
  with a better  choice
  - add an interface to load, save and close sight files

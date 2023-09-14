6330733b2ac4a6e716e82abff190adcd27a7103b
Michele <michele.castellana@gmail.com>
2023-09-14 16:16:01 +0200

    Changed note

--- main.cpp
+++ main.cpp
@@ -23,7 +23,6 @@
  
  ********** THINGS TO ADD ************
  
- - in SaveAndReset<F>::operator the file write dialog window should be the big one
  - when one double clicks on a sight/route/position in ListFrame, open the modify panel
  - fill up selection 'rectangle' in 3D projection by using a bundle of curves at fixed lat or lon
  - change     highlight_color = color_selected_item and     highlight_font = default_font;

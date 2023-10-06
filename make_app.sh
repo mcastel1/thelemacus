97be38c53016b11b5326587c89d15833ffaffe08
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-06 22:00:13 +0200

    It works

--- make_app.sh
+++ make_app.sh
@@ -13,20 +13,21 @@ APP_NAME='Thelemacus'
 #the path to the icon of the app
 ICON_PATH='/Users/macbookpro/Documents/sight_reduction_program/jolly_roger.icns'
 #the path to the .cpp file
-CPP_FILE_PATH='/Users/macbookpro/Documents/sight_reduction_program'
+INPUT_PATH='/Users/macbookpro/Documents/sight_reduction_program'
+OUTPUT_PATH='/Users/macbookpro/Desktop'
 
-rm -rf temp.o $APP_NAME.app $APP_NAME
+rm -rf temp.o $OUTPUT_PATH/$APP_NAME.app $APP_NAME
 
 
-/Applications/wxWidgets-3.2.2.1/build-cocoa-debug/bk-deps g++ `wx-config --cxxflags --libs` -std=gnu++11 -mmacosx-version-min=10.10 -c -O3 -o temp.o -I/Applications/wxWidgets-3.2.2.1/build-cocoa-debug/lib/wx/include/osx_cocoa-unicode-3.2 -I/usr/local/include/gsl/ -I/Applications/boost_1_66_0/ -I../../../include -D_FILE_OFFSET_BITS=64  -D__WXOSX_COCOA__      -I/Users/macbookpro/Documents/sight_reduction_program/ -DWXUSINGDLL -I../../../samples/minimal/../../samples -Wall -Wundef -Wunused-parameter -Wno-ctor-dtor-privacy -Woverloaded-virtual -Wno-deprecated-declarations -g -O0 -I/usr/local/Cellar/pcre2/10.39/include -fno-common  -fvisibility=hidden -fvisibility-inlines-hidden -dynamic -fPIC -DPIC   $CPP_FILE_PATH/main.cpp
+/Applications/wxWidgets-3.2.2.1/build-cocoa-debug/bk-deps g++ `wx-config --cxxflags --libs` -std=gnu++11 -mmacosx-version-min=10.10 -c -O3 -o temp.o -I/Applications/wxWidgets-3.2.2.1/build-cocoa-debug/lib/wx/include/osx_cocoa-unicode-3.2 -I/usr/local/include/gsl/ -I/Applications/boost_1_66_0/ -I../../../include -D_FILE_OFFSET_BITS=64  -D__WXOSX_COCOA__      -I/Users/macbookpro/Documents/sight_reduction_program/ -DWXUSINGDLL -I../../../samples/minimal/../../samples -Wall -Wundef -Wunused-parameter -Wno-ctor-dtor-privacy -Woverloaded-virtual -Wno-deprecated-declarations -g -O0 -I/usr/local/Cellar/pcre2/10.39/include -fno-common  -fvisibility=hidden -fvisibility-inlines-hidden -dynamic -fPIC -DPIC   $INPUT_PATH/main.cpp
 g++ `wx-config --cxxflags --libs`  -std=gnu++11 -mmacosx-version-min=12.0 -o $APP_NAME  temp.o    -L/Applications/wxWidgets-3.2.2.1/build-cocoa-debug/lib      -framework IOKit -framework Carbon -framework Cocoa -framework QuartzCore -framework AudioToolbox -framework System -framework OpenGL  -lwx_osx_cocoau_core-3.2  -lwx_baseu-3.2        -lpng -lz -ljpeg -L/usr/local/Cellar/libtiff/4.3.0/lib -ltiff -framework WebKit  -lgsl -lcblas  -framework IOKit -framework Carbon -framework Cocoa -framework QuartzCore -framework AudioToolbox -framework System -framework OpenGL  -lz -framework Security -lpthread -liconv -lcurl -L/usr/local/Cellar/pcre2/10.39/lib -lpcre2-32  -lz -framework Security -lpthread -liconv -lcurl
-mkdir -p $APP_NAME.app/Contents
-mkdir -p $APP_NAME.app/Contents/MacOS
-mkdir -p $APP_NAME.app/Contents/Resources/Data
-mkdir -p $APP_NAME.app/Contents/Resources/Images
+mkdir -p $OUTPUT_PATH/$APP_NAME.app/Contents
+mkdir -p $OUTPUT_PATH/$APP_NAME.app/Contents/MacOS
+mkdir -p $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/Data
+mkdir -p $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/Images
 
-cp -r $CPP_FILE_PATH/Contents/Resources/Data/* $APP_NAME.app/Contents/Resources/Data
-cp -r $CPP_FILE_PATH/Contents/Resources/Images/* $APP_NAME.app/Contents/Resources/Images
+cp -r $INPUT_PATH/Contents/Resources/Data/* $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/Data
+cp -r $INPUT_PATH/Contents/Resources/Images/* $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/Images
 
 sed -e "s/\${MACOSX_BUNDLE_GUI_IDENTIFIER}/org.wxwidgets.$APP_NAME/" \
     -e "s/\${MACOSX_BUNDLE_EXECUTABLE_NAME}/$APP_NAME/" \
@@ -36,9 +37,9 @@ sed -e "s/\${MACOSX_BUNDLE_GUI_IDENTIFIER}/org.wxwidgets.$APP_NAME/" \
     -e "s/\${MACOSX_BUNDLE_INFO_STRING}/$APP_NAME version 3.2.0, (c) 2002-2022 wxWidgets/" \
     -e "s/\${MACOSX_BUNDLE_LONG_VERSION_STRING}/3.2.0, (c) 2002-2022 wxWidgets/" \
     -e "s/\${MACOSX_BUNDLE_SHORT_VERSION_STRING}/3.2/" \
-    /Applications/wxWidgets-3.2.2.1/src/osx/carbon/Info.plist.in >$APP_NAME.app/Contents/Info.plist
-/bin/echo "APPL????" >$APP_NAME.app/Contents/PkgInfo
-ln -f $APP_NAME $APP_NAME.app/Contents/MacOS/$APP_NAME
-cp -f $ICON_PATH $APP_NAME.app/Contents/Resources/wxmac.icns
+    /Applications/wxWidgets-3.2.2.1/src/osx/carbon/Info.plist.in >$OUTPUT_PATH/$APP_NAME.app/Contents/Info.plist
+/bin/echo "APPL????" >$OUTPUT_PATH/$APP_NAME.app/Contents/PkgInfo
+ln -f $APP_NAME $OUTPUT_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME
+cp -f $ICON_PATH $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/wxmac.icns
 
 rm -rf temp.o $APP_NAME

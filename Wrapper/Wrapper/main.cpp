252bdf7682cdab3df2ec72d7a45fb0cf1f3b5b7e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-17 16:46:10 +0200

    main.cpp manages to export DYLD_LIBRARY_PATH -> Make it run thelemacus

--- Wrapper/Wrapper/main.cpp
+++ Wrapper/Wrapper/main.cpp
@@ -13,6 +13,9 @@
 int main(int argc, const char * argv[]) {
     
     // insert code here...
+    
+    system("export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:/Users/macbookpro/Desktop/Thelemacus.app/Contents/Resources/Libraries/; echo \$DYLD_LIBRARY_PATH");
+
     std::cout << "Hello, World!\n";
     return 0;
     

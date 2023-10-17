0bde546014d93a6b41e64cab635fc0d7e1cf0e86
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-17 16:39:50 +0200

    Added libraries .h

--- Wrapper/Wrapper/main.cpp
+++ Wrapper/Wrapper/main.cpp
@@ -1,14 +1,19 @@
-//
-//  main.cpp
-//  Wrapper
-//
-//  Created by MacBook Pro on 17/10/2023.
-//
-
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <list>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+    
 }

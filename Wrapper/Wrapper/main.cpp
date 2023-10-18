bb6f3e781287b0917c3ee0155a7379f008dfc0d4
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-18 11:49:08 +0200

    Moved path.dat into running directory

--- Wrapper/Wrapper/main.cpp
+++ Wrapper/Wrapper/main.cpp
@@ -19,12 +19,13 @@ int main(int argc, const char * argv[]) {
     stringstream library_directory, command;
     string running_directory;
     
+    //run pwd and get the current running path of this .o -> write it into path.dat so the app can read it later
     system("rm -rf path.dat; pwd >> path.dat;");
     infile.open("path.dat");
     running_directory.clear();
     getline(infile, running_directory);
     infile.close();
-    system("rm -rf path.dat");
+//    system("rm -rf path.dat");
     
     cout << "Running directory = " << running_directory << "\n";
     
@@ -33,10 +34,12 @@ int main(int argc, const char * argv[]) {
 
     cout << "Library directory = " << library_directory.str().c_str() << "\n";
 
+    //export the libraries so the app knows where to find them later
     command.clear();
     command << "export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:"
             << library_directory.str().c_str()
             << "; echo \$DYLD_LIBRARY_PATH";
+//            << "; mv path.dat " << running_directory;
     
     cout << "Command  = " << command.str().c_str() << "\n";
     

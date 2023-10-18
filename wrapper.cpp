a96a10700781b60edf7add173b118959693c2133
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-18 12:08:03 +0200

    It finds and uses the libraries installed locally in /Thelemacus.app/ ... / Libraries ! I have to run from command line ./Thelemacus in /MacOS/ because if I just click on the App icon it runs in /mabookpro and it cannot find its own path -> Fix this with where_am_i

---
+++ wrapper.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <list>
+#include <stdio.h>
+
+/*
+ compile on mac with
+ 
+ g++ wrapper.cpp -o wrapper.o -Wall -Wno-c++11-extensions --std=c++17  -O3
+ 
+ */
+
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    // insert code here...
+    ifstream infile;
+    stringstream library_directory, command;
+    string running_directory;
+    
+    //run pwd and get the current running path of this .o -> write it into path.dat so the app can read it later
+    system("rm -rf path.dat; pwd >> path.dat;");
+    infile.open("path.dat");
+    running_directory.clear();
+    getline(infile, running_directory);
+    infile.close();
+//    system("rm -rf path.dat");
+    
+    cout << "Running directory = " << running_directory << "\n";
+    
+    library_directory.clear();
+    library_directory << running_directory << "/../Resources/Libraries/";
+
+    cout << "Library directory = " << library_directory.str().c_str() << "\n";
+
+    //export the libraries so the app knows where to find them later
+    command.clear();
+    command << "export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:"
+            << library_directory.str().c_str()
+            << "; echo \$DYLD_LIBRARY_PATH"
+            << "; ./main.o";
+    
+    cout << "Command  = " << command.str().c_str() << "\n";
+    
+
+    
+    system(command.str().c_str());
+
+    return 0;
+    
+}

a123bf8f5141317ff72de0c2457e7663ef6ef992
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-24 13:50:00 +0200

    Added other stuff to empty and fill. sh

--- fill.sh
+++ fill.sh
@@ -6,4 +6,5 @@ mv $SAVE_PATH/libwx* $SAVE_PATH/libtiff.5.dylib $SAVE_PATH/libgsl* $SAVE_PATH/li
 mv $SAVE_PATH/libboost_filesystem.dylib $SAVE_PATH/libboost_system.dylib /usr/local/opt/boost/lib/
 #cp $SAVE_PATH/libpng16.16.dylib /usr/local/opt/libpng/lib/
 mv $SAVE_PATH/libpng16.16.dylib /usr/local/lib/
+mv $SAVE_PATH/liblzma.5.dylib /usr/local/opt/xz/lib/
 cp  /usr/local/lib/libpng16.16.dylib /usr/local/opt/libpng/lib/

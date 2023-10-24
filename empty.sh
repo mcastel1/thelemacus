0261898c798d2b4e2f5bdaa0e8f0b8820d922cc2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2023-10-24 11:29:49 +0200

    Included variable SAVE_PATH in fill and empty.sh

--- empty.sh
+++ empty.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mv /usr/local/lib/libwx* /usr/local/lib/libtiff.5.dylib /usr/local/lib/libgsl* /usr/local/lib/libjpeg.9.dylib /usr/local/lib/libpcre2-32.0.dylib /usr/local/lib/libpng16.16.dylib  ~/Desktop/saved
-mv /usr/local/opt/boost/lib/libboost_filesystem.dylib /usr/local/opt/boost/lib/libboost_system.dylib ~/Desktop/saved
+SAVE_PATH=/Users/macbookpro/Desktop/saved
+
+mv /usr/local/lib/libwx* /usr/local/lib/libtiff.5.dylib /usr/local/lib/libgsl* /usr/local/lib/libjpeg.9.dylib /usr/local/lib/libpcre2-32.0.dylib /usr/local/lib/libpng16.16.dylib  $SAVE_PATH
+mv /usr/local/opt/boost/lib/libboost_filesystem.dylib /usr/local/opt/boost/lib/libboost_system.dylib $SAVE_PATH
+mv /usr/local/opt/libpng/lib/libpng16.16.dylib $SAVE_PATH
--- fill.sh
+++ fill.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mv ~/Desktop/saved/libwx* ~/Desktop/saved/libtiff.5.dylib ~/Desktop/saved/libgsl* ~/Desktop/saved/libjpeg.9.dylib ~/Desktop/saved/libpcre2-32.0.dylib ~/Desktop/saved/libpng16.16.dylib  /usr/local/lib/
-mv ~/Desktop/saved/libboost_filesystem.dylib ~/Desktop/saved/libboost_system.dylib /usr/local/opt/boost/lib/
+SAVE_PATH=/Users/macbookpro/Desktop/saved
+
+mv $SAVE_PATH/libwx* $SAVE_PATH/libtiff.5.dylib $SAVE_PATH/libgsl* $SAVE_PATH/libjpeg.9.dylib $SAVE_PATH/libpcre2-32.0.dylib $SAVE_PATH/libpng16.16.dylib  /usr/local/lib/
+mv $SAVE_PATH/libboost_filesystem.dylib $SAVE_PATH/libboost_system.dylib /usr/local/opt/boost/lib/
+mv $SAVE_PATH/libpng16.16.dylib /usr/local/opt/libpng/lib/

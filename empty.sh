78eaf41599bc9db6563068a62411fe73bce02052
mcastel1 <michele.castellana@gmail.com>
2023-10-26 15:04:17 +0200

    updated empty and fill

--- empty.sh
+++ empty.sh
@@ -3,7 +3,7 @@
 SAVE_PATH=/Users/macbookpro/saved
 
 mv /usr/local/lib/libwx* /usr/local/lib/libtiff.5.dylib /usr/local/lib/libgsl* /usr/local/lib/libjpeg.9.dylib /usr/local/lib/libpcre2-32.0.dylib /usr/local/lib/libpng16.16.dylib  $SAVE_PATH
-mv /usr/local/opt/boost/lib/libboost_filesystem.dylib /usr/local/opt/boost/lib/libboost_system.dylib $SAVE_PATH
+mv /usr/local/opt/boost/lib/libboost_*.dylib  $SAVE_PATH
 mv /usr/local/opt/libpng/lib/libpng16.16.dylib $SAVE_PATH
 mv /usr/local/opt/xz/lib/liblzma.5.dylib $SAVE_PATH
 #mv /usr/local/lib/libpng16.16.dylib $SAVE_PATH
--- fill.sh
+++ fill.sh
@@ -3,7 +3,7 @@
 SAVE_PATH=/Users/macbookpro/saved
 
 mv $SAVE_PATH/libwx* $SAVE_PATH/libtiff.5.dylib $SAVE_PATH/libgsl* $SAVE_PATH/libjpeg.9.dylib $SAVE_PATH/libpcre2-32.0.dylib $SAVE_PATH/libpng16.16.dylib  /usr/local/lib/
-mv $SAVE_PATH/libboost_filesystem.dylib $SAVE_PATH/libboost_system.dylib /usr/local/opt/boost/lib/
+mv $SAVE_PATH/libboost_*.dylib /usr/local/opt/boost/lib/
 #cp $SAVE_PATH/libpng16.16.dylib /usr/local/opt/libpng/lib/
 mv $SAVE_PATH/libpng16.16.dylib /usr/local/lib/
 mv $SAVE_PATH/liblzma.5.dylib /usr/local/opt/xz/lib/

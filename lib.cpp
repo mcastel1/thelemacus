cb1f49ae14f2f4cb5e640d5377578e85a03f863a
mcastel1 <michele.castellana@gmail.com>
2023-11-03 13:49:33 +0100

    comments

--- lib.cpp
+++ lib.cpp
@@ -16804,15 +16804,66 @@ OnChangeSelectionInLimbField::OnChangeSelectionInLimbField(LimbField* caller_in)
 
 //when an item is selected/deselcted in *caller, make sure that only one item stays selected in *calller
 template<class T>void OnChangeSelectionInLimbField::operator()(T& event){
+    
+    wxArrayInt temp;
+    long i, j;
         
-    caller->checked_items.Clear();
-    caller->name_new->GetCheckedItems(caller->checked_items);
+    temp.Clear();
+    caller->name_new->GetCheckedItems(temp);
 
-    cout << "checked_items : ";
-    for(int i=0; i<caller->checked_items.GetCount(); ++i){
-        cout << " " << caller->checked_items.Item(i);
+     
+    if((temp.GetCount()) <= 1){
+        //only one item is selected -> write temp into caller->selected_items
+        
+        caller->checked_items.Clear();
+        (caller->checked_items) = temp;
+        
+    }else{
+        //multiple items are selecte -> check for the newly selected item, and write it as the only element  into checked_items
+        
+        for(i=0; i<temp.GetCount(); i++){
+            
+            for(j=0; (j<(caller->checked_items).GetCount()) && ((caller->checked_items.Item(j)) != (temp.Item(i))); j++){}
+            
+            if(j == (caller->checked_items).GetCount()){
+                //the item temp.Item(i) is not present in (caller->checked_items) -> resize caller->checked_items to size one and write into its only element temp.Item(i)
+                
+                caller->checked_items.Clear();
+                caller->checked_items.Add(temp.Item(i), 1);
+                
+                break;
+                
+            }
+        }
+        
     }
-    cout << "\n";
+    
+    //update the checked items in caller->name_new
+    if((caller->checked_items.GetCount()) == 0){
+        //checked_items is empty->uncheck everythig in name_new
+        
+        for(i=0; i<(caller->limbs.GetCount()); i++){
+            caller->name_new->Check(((unsigned int)i), false);
+        }
+        
+    }else{
+        //checked_items is not empty->check, in name_new, only the first element in checked_items
+        
+        for(i=0; i<(caller->limbs.GetCount()); i++){
+            caller->name_new->Check(((unsigned int)i), (i == (caller->checked_items.Item(0))));
+        }
+        
+    }
+    
+  
+        
+        
+//    cout << "checked_items : ";
+//    for(int i=0; i<caller->checked_items.GetCount(); ++i){
+//        cout << " " << caller->checked_items.Item(i);
+//    }
+//    cout << "\n";
+
     
     
     event.Skip(true);

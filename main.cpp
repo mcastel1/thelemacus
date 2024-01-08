7517471837a988aa997730884159bc3355ac18c8
mcastel1 <michele.castellana@gmail.com>
2024-01-08 15:07:10 +0100

    note

--- main.cpp
+++ main.cpp
@@ -72,14 +72,14 @@ To run on WIN32, the resource file is resource_file_windows.rc
  - move all stuff which is general enough in the code to MyApp class
 
  ********** THINGS TO FIX ************
+ - fix template<class T> void ChartFrame::KeyDown(T& event) {
  - add the ::check method to Route and PositionFrame, and bind it as it is bound in SightFrame
  - implement the read-from-file structure used for Data also for the other composite objects such as Body etc
  - listcontrol_sights etc are not resized to their minimum sizes 
  - check that WXK_PLUS does not depend on the computer
- ---
 
  for MSW:
- - mouse hovering over listcontrol does not work well 
+ - mouse hovering over listcontrol does not work well
  - esc key does not work in SightFrame and other *Frames when one does not FOCUS on a control
  - when you open sample_sight.nav from resouces, you open it in R mode -> when you try to write in it you will get an error
  - center name of file in top bar of ListFrame

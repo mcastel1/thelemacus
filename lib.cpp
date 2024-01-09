6fc163f1825511fd0f93ec104b9feab1eaa995d6
mcastel1 <michele.castellana@gmail.com>
2024-01-09 12:11:15 +0100

    Fixed bug with 'do you want to ... ?'

--- lib.cpp
+++ lib.cpp
@@ -12892,8 +12892,9 @@ template <class T> void ResetListFrame::operator()(T& event) {
 
 	//clear p->data and allocate a new one
 	(p->data)->~Data();
-	//the file now has no title
+	//the file now has no title and has not been modified
 	(p->file_is_untitled) = true;
+    (p->file_has_been_modified) = false;
 
 	p->data = new Data(p->catalog, String(""));
 

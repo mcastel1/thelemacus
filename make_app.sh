467369b4f63bc7189bd6829c64d929f00ed47969
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-01-17 22:45:12 +0100

    Solved linking libpng16.16.dylib -> libz.1.dylib. Now I know how to correctly link the libraries -> Do the same for all other linkings

--- make_app.sh
+++ make_app.sh
@@ -61,7 +61,7 @@ install_name_tool -add_rpath @executable_path/../Resources/Libraries/ $APP_NAME
 #install_name_tool -change /Applications/wxWidgets-$WX_VERSION/build-cocoa-debug/lib/libwx_osx_cocoau_core-3.2.0.2.2.dylib @rpath/libwx_osx_cocoau_core-3.2.0.2.2.dylib $APP_NAME
 #install_name_tool -change /Applications/wxWidgets-$WX_VERSION/build-cocoa-debug/lib/libwx_baseu-3.2.0.2.2.dylib @rpath/libwx_baseu-3.2.0.2.2.dylib $APP_NAME
 install_name_tool -change $LIBPNG_LIB_DIRECTORY/libpng16.16.dylib @rpath/libpng16.16.dylib $APP_NAME
-#install_name_tool -change /usr/lib/libz.1.dylib @rpath/libz.1.dylib $APP_NAME
+install_name_tool -change /usr/lib/libz.1.dylib @rpath/libz.1.dylib $APP_NAME
 #install_name_tool -change $LIBJPEG_LIB_DIRECTORY/libjpeg.9.dylib @rpath/libjpeg.9.dylib $APP_NAME
 #install_name_tool -change $LIBTIFF_LIB_DIRECTORY/libtiff.6.dylib @rpath/libtiff.6.dylib $APP_NAME
 #install_name_tool -change /usr/lib/libiconv.2.dylib @rpath/libiconv.2.dylib $APP_NAME
@@ -86,12 +86,20 @@ cp $BOOST_LIB_DIRECTORY/libboost_filesystem.dylib  $APP_LIBRARY_DIRECTORY
 cp $BOOST_LIB_DIRECTORY/libboost_system.dylib $APP_LIBRARY_DIRECTORY
 cp $BOOST_LIB_DIRECTORY/libboost_atomic.dylib $APP_LIBRARY_DIRECTORY
 cp $LIBPNG_LIB_DIRECTORY/libpng16.16.dylib $APP_LIBRARY_DIRECTORY
+cp $LIBZ_LIB_DIRECTORY/libz.1.dylib $APP_LIBRARY_DIRECTORY
 #
 ##libraries for main.o (removed /usr/lib/libc++.1.dylib /usr/lib/libSystem.B.dylib /usr/lib/dyld $BOOST_LIB_DIRECTORY/libboost_filesystem.dylib   $BOOST_LIB_DIRECTORY/libboost_system.dylib because they are alreday in the libraries for $APP_NAME)
 ##/System/Library/Frameworks/IOKit.framework/Versions/A/IOKit  /System/Library/Frameworks/Carbon.framework/Versions/A/Carbon  /System/Library/Frameworks/Cocoa.framework/Versions/A/Cocoa  /System/Library/Frameworks/QuartzCore.framework/Versions/A/QuartzCore  /System/Library/Frameworks/AudioToolbox.framework/Versions/A/AudioToolbox     /System/Library/Frameworks/OpenGL.framework/Versions/A/OpenGL /System/Library/Frameworks/WebKit.framework/Versions/A/WebKit /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib /usr/local/opt/libpng/lib/libpng16.16.dylib /System/Library/Frameworks/Security.framework/Versions/A/Security /usr/local/opt/xz/lib/liblzma.5.dylib
-#cp   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_xrc-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_html-3.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_qa-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_core-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_baseu_xml-3.2.dylib   $WXWIDGETS_LIB_DIRECTORY//libwx_baseu_net-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.dylib     /Applications/wxWidgets-$WX_VERSION/build-cocoa-debug/lib/libwx_baseu-3.2.0.2.2.dylib      $LIBZ_LIB_DIRECTORY/libz.1.dylib   $LIBJPEG_LIB_DIRECTORY/libjpeg.9.dylib   $LIBTIFF_LIB_DIRECTORY/libtiff.6.dylib      $GSL_LIB_DIRECTORY/libgsl.27.dylib         $LIBICONV_LIB_DIRECTORY/libiconv.2.dylib  $LIBCURL_LIB_DIRECTORY/libcurl.4.dylib   $LIBPCRE2_LIB_DIRECTORY/libpcre2-32.0.dylib  $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_html-3.2.0.2.2.dylib  $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.0.2.2.dylib $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.0.2.2.dylib $WXWIDGETS_LIB_DIRECTORY/libwx_baseu_xml-3.2.0.2.2.dylib     $APP_LIBRARY_DIRECTORY
+#cp   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_xrc-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_html-3.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_qa-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_core-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_baseu_xml-3.2.dylib   $WXWIDGETS_LIB_DIRECTORY//libwx_baseu_net-3.2.0.2.2.dylib   $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.dylib     /Applications/wxWidgets-$WX_VERSION/build-cocoa-debug/lib/libwx_baseu-3.2.0.2.2.dylib    $LIBJPEG_LIB_DIRECTORY/libjpeg.9.dylib   $LIBTIFF_LIB_DIRECTORY/libtiff.6.dylib      $GSL_LIB_DIRECTORY/libgsl.27.dylib         $LIBICONV_LIB_DIRECTORY/libiconv.2.dylib  $LIBCURL_LIB_DIRECTORY/libcurl.4.dylib   $LIBPCRE2_LIB_DIRECTORY/libpcre2-32.0.dylib  $WXWIDGETS_LIB_DIRECTORY/libwx_osx_cocoau_html-3.2.0.2.2.dylib  $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.0.2.2.dylib $WXWIDGETS_LIB_DIRECTORY/libwx_baseu-3.2.0.2.2.dylib $WXWIDGETS_LIB_DIRECTORY/libwx_baseu_xml-3.2.0.2.2.dylib     $APP_LIBRARY_DIRECTORY
 #
 #
+
+
+
+##libraries called by libpng16.16.dylib
+install_name_tool -add_rpath @executable_path/../Resources/Libraries/ $APP_LIBRARY_DIRECTORY/libpng16.16.dylib
+install_name_tool -change /usr/lib/libz.1.dylib @rpath/libz.1.dylib  $APP_LIBRARY_DIRECTORY/libpng16.16.dylib
+
 
 ##libraries called by libwx_osx_cocoau_xrc-3.2.0.2.2.dylib
 #install_name_tool -add_rpath @executable_path/../Resources/Libraries/ $APP_LIBRARY_DIRECTORY/libwx_osx_cocoau_xrc-3.2.0.2.2.dylib

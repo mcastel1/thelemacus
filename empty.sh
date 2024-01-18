1b06bd086358cf7d9029561d4b80b70df9350efc
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-01-18 13:22:34 +0100

    Revised fill and empty.sh

--- empty.sh
+++ empty.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-SAVE_PATH=/Users/macbookpro/saved
+SAVE_PATH=/Users/michele/saved
+WXWIDGETS_LIB_DIRECTORY='/Applications/wxWidgets-3.2.4/build-cocoa-debug/lib'
 
-mv /usr/local/lib/libwx* /usr/local/lib/libtiff.5.dylib /usr/local/lib/libgsl* /usr/local/lib/libjpeg.9.dylib /usr/local/lib/libpcre2-32.0.dylib /usr/local/lib/libpng16.16.dylib  $SAVE_PATH
-mv /usr/local/opt/boost/lib/libboost_*.dylib  $SAVE_PATH
-mv /usr/local/opt/libpng/lib/libpng16.16.dylib $SAVE_PATH
-mv /usr/local/opt/xz/lib/liblzma.5.dylib $SAVE_PATH
-#mv /usr/local/lib/libpng16.16.dylib $SAVE_PATH
+mv $WXWIDGETS_LIB_DIRECTORY/libwx*   $SAVE_PATH
--- fill.sh
+++ fill.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-SAVE_PATH=/Users/macbookpro/saved
+SAVE_PATH=/Users/michele/saved
+WXWIDGETS_LIB_DIRECTORY='/Applications/wxWidgets-3.2.4/build-cocoa-debug/lib'
 
-mv $SAVE_PATH/libwx* $SAVE_PATH/libtiff.5.dylib $SAVE_PATH/libgsl* $SAVE_PATH/libjpeg.9.dylib $SAVE_PATH/libpcre2-32.0.dylib $SAVE_PATH/libpng16.16.dylib  /usr/local/lib/
-mv $SAVE_PATH/libboost_*.dylib /usr/local/opt/boost/lib/
-#cp $SAVE_PATH/libpng16.16.dylib /usr/local/opt/libpng/lib/
-mv $SAVE_PATH/libpng16.16.dylib /usr/local/lib/
-mv $SAVE_PATH/liblzma.5.dylib /usr/local/opt/xz/lib/
-cp  /usr/local/lib/libpng16.16.dylib /usr/local/opt/libpng/lib/
+mv $SAVE_PATH/libwx* $WXWIDGETS_LIB_DIRECTORY

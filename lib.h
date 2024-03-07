2ad5df39b136485530f9dcebca929a11e8c17a9b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-03-07 09:24:51 +0100

    Now I am doing the same for ProjectionField ...

--- lib.h
+++ lib.h
@@ -2030,13 +2030,13 @@ template<class P> class ProjectionField: public MultipleItemField<P>{
     
 public:
     
-    P* parent;
-    wxBoxSizer *sizer_h, *sizer_v;
-    wxArrayString  /*this is equal to a standard list of the available graphical types*/items, /*same as projections, but it is fixed and never wrote to: it is a fixed catalog*/catalog;
+//    P* parent;
+//    wxBoxSizer *sizer_h, *sizer_v;
+//    wxArrayString  /*this is equal to a standard list of the available graphical types*/items, /*same as projections, but it is fixed and never wrote to: it is a fixed catalog*/catalog;
     //this is the wxComboBox with the name of the projections
-    wxComboBox* name;
-    vector<int> recent_items;
-    bool ok;
+//    wxComboBox* name;
+//    vector<int> recent_items;
+//    bool ok;
     CheckProjection<P>* check;
 
     ProjectionField(wxPanel*);

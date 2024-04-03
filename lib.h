fb46678a28a0d5d318b40e82dbd7e26a5a8ce5a2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-04-03 14:38:35 +0200

    Fixed but with two ChartTransportHandlers

--- lib.h
+++ lib.h
@@ -2006,7 +2006,7 @@ public:
     void PreRenderMercator(void);
     void PreRender3D(void);
     void PaintEvent(wxPaintEvent&);
-    void PaintNow(void);
+//    void PaintNow(void);
     void RenderAll(wxDC&);
     void MyRefresh(void);
     void RefreshWIN32(void);

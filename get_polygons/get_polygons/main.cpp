62c6d54045b167f2f422e372420e1f517241fc1a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-04-19 15:46:39 +0200

    Created get_polygons, with which I will extract the polyogns from the .kml file

---
+++ get_polygons/get_polygons/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  get_polygons
+//
+//  Created by Michele on 19/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

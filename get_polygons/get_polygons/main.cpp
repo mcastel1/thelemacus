20e7c479b4b23cc2bfb1635a719780d26c30203f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-04-19 16:34:47 +0200

    If in Render_Mercator I plot coastlines with
        for (i = 0; i < size_points_coastline-1; i++) {
            dc->DrawLine(points_coastline[i], points_coastline[i+1]);
        }
    
    I get an accurate coastline with a fluid movement of the chart -> It is probably worthed trying polygons :)

--- get_polygons/get_polygons/main.cpp
+++ get_polygons/get_polygons/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+#include <cstdio>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <list>
+
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
+    
+    ifstream infile;
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+    
 }

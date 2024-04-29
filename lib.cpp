d58c5ae5c5e6e48660c743fef52e4c03233b6513
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-04-29 15:15:09 +0200

    Created coastline_polygons_map_rectangle_observer

--- lib.cpp
+++ lib.cpp
@@ -8550,6 +8550,18 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
         every = ((unsigned long long int)(((double)(parent->n_all_coastline_points)) * ( ( (draw_panel->x_span()) * (p_NE.y - p_SW.y) ) / ( (draw_panel->x_span_0)  *(p_NE0.y - p_SW0.y) ) ) / ((double)(wxGetApp().n_points_plot_coastline_Mercator.value))));
         if(every==0){every = 1;}
         
+        
+        
+        for(parent->coastline_polygons_map_rectangle_observer.clear(), i=i_min; i<i_max; i++) {
+            for(j=j_min; j<j_max; j++) {
+                
+                for(l=0; l<(parent->coastline_polygons_map)[i+floor_min_lat][j % 360].size(); l++){
+                    parent->coastline_polygons_map_rectangle_observer.push_back((parent->coastline_polygons_map)[i+floor_min_lat][j % 360][l]);
+                }
+                
+            }
+        }
+        
         for(p=0, i=0, l=0, n_added_polygons=0, polygon_position_now.clear(); i<parent->coastline_polygons_Position.size(); i++) {
             //run through polygons
             
@@ -8567,8 +8579,6 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
                     //(parent->coastline_polygons_Position)[i][j] is a valid point
                     
                     coastline_polygons_now[l++] = q;
-//                    polygon_position_now[i]++;
-                    
                     new_polygon = false;
                     
                 }else{
@@ -8598,6 +8608,11 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
             polygon_position_now.resize(n_added_polygons+1);
         }
         polygon_position_now.back() = l;
+        //
+        
+        
+        
+        
         
 
         

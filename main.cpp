255d3391f44ed0a9e888d9597fc5e224b11837b9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-05-16 12:57:30 +0200

    Fixed bug with ts-> Now phi_min/max works for orthodromes

--- main.cpp
+++ main.cpp
@@ -245,13 +245,12 @@ void MyApp::where_am_I([[maybe_unused]] String prefix) {
 
 bool MyApp::OnInit() {
     
-    //
+    /*
     Route r;
     Angle phi_min, phi_max;
-    r = Route(Route_types[1], Position(Angle(2.32), Angle(1.13)), Angle(2.43), Length(3000.32));
+    r = Route(Route_types[1], Position(Angle(2.32), Angle(1.13)), Angle(.43), Length(3000.32));
     r.phi_min_max(&phi_min, &phi_max, String(""));
-    
-    //
+    */
     
     
     unsigned int i;

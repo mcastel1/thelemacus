e74685a9b26836e369dd37b8af8ece88771bc613
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-05-21 21:40:55 +0200

    removed useless parentheses

--- lib.cpp
+++ lib.cpp
@@ -9502,8 +9502,8 @@ inline void DrawPanel::RenderSelectionRectangle(wxDC& dc,
     temp = (Route(
                   RouteType(((Route_types[0]).value)),
                (parent->parent->geo_position_start),
-               Angle(M_PI_2 + M_PI * (1.0 + GSL_SIGN((normalize_pm_pi_ret(geo_position.lambda).value) - ((((parent->parent->geo_position_start).lambda).normalize_pm_pi_ret()).value))) / 2.0),
-               Length(Re * cos((parent->parent->geo_position_start).phi) * fabs((normalize_pm_pi_ret(geo_position.lambda).value) - ((((parent->parent->geo_position_start).lambda).normalize_pm_pi_ret()).value)))
+               Angle(M_PI_2 + M_PI * (1.0 + GSL_SIGN((normalize_pm_pi_ret(geo_position.lambda).value) - (parent->parent->geo_position_start.lambda.normalize_pm_pi_ret().value))) / 2.0),
+               Length(Re * cos(parent->parent->geo_position_start.phi) * fabs((normalize_pm_pi_ret(geo_position.lambda).value) - ((parent->parent->geo_position_start.lambda.normalize_pm_pi_ret()).value)))
                   ));
     
     temp.DrawOld((wxGetApp().n_points_routes.value), &dc, this, String(""));
@@ -9513,8 +9513,8 @@ inline void DrawPanel::RenderSelectionRectangle(wxDC& dc,
            RouteType(((Route_types[0]).value)),
         geo_position,
         //change this by introducing if
-        Angle(M_PI_2 + M_PI * (1.0 - GSL_SIGN((normalize_pm_pi_ret(geo_position.lambda).value) - ((((parent->parent->geo_position_start).lambda).normalize_pm_pi_ret()).value))) / 2.0),
-        Length(Re * cos(geo_position.phi) * fabs((normalize_pm_pi_ret(geo_position.lambda).value) - ((((parent->parent->geo_position_start).lambda).normalize_pm_pi_ret()).value)))
+        Angle(M_PI_2 + M_PI * (1.0 - GSL_SIGN((normalize_pm_pi_ret(geo_position.lambda).value) - (parent->parent->geo_position_start.lambda.normalize_pm_pi_ret().value))) / 2.0),
+        Length(Re * cos(geo_position.phi) * fabs((normalize_pm_pi_ret(geo_position.lambda).value) - ((parent->parent->geo_position_start.lambda.normalize_pm_pi_ret()).value)))
     )).DrawOld((wxGetApp().n_points_routes.value), &dc, this, String(""));
 
 

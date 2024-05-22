c659b8e24b1525768a5427881a1a384d473316ce
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-05-22 12:44:12 +0200

    Coded case where lamdba_a is in the + hemisphere

--- lib.cpp
+++ lib.cpp
@@ -9522,6 +9522,10 @@ inline void DrawPanel::RenderSelectionRectangle(wxDC& dc,
         if((lambda_a.normalize_pm_pi_ret().value) >= 0.0){
             //lambda_a lies in the poisitive-logitude hemishere (0 < lambda < 180), lambda_b in the nevative-longitude hemisphere (180 < lambda < 360)
             
+            l = Length(Re * cos(parent->parent->geo_position_start.phi) * fabs((lambda_a.normalize_pm_pi_ret().value) - (lambda_b.normalize_pm_pi_ret().value)));
+            Z = Angle(M_PI_2);
+
+            
         }else{
             
             

818f80b6a3ec2cedefa8b7ef3950082fb758b411
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-05-23 11:50:24 +0200

    Now the 'existing route' button is disabled when tehre are no routes available for transport

--- lib.cpp
+++ lib.cpp
@@ -19362,7 +19362,7 @@ void ListFrame::OnTransportSight(wxCommandEvent& event) {
     }
     
     
-    print_question->SetAndCall(NULL, String(""), String("You want to transport a sight. With what route do you want to transport?"), String("Existing route"), String("New route"), (n_routes_for_transport > 0 ? true : false), false);
+    print_question->SetAndCall(NULL, String(""), String("You want to transport a sight. With what route do you want to transport?"), String("Existing route"), String("New route"), (n_routes_for_transport > 0 ? true : false), true);
    
 
     OnModifyFile();

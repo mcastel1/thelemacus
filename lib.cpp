3e3d529ad68e220aa0758431d1861a61c9841bca
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-04 11:31:21 +0200

    Convert Unit is now bound to  wxEVT_KEY_UP too

--- lib.cpp
+++ lib.cpp
@@ -22148,7 +22148,7 @@ template<class P> StaticLengthField<P>::StaticLengthField(wxPanel* panel_of_pare
 
     
     LengthField<P>::unit->name->Bind(wxEVT_COMBOBOX, &StaticLengthField<P>:: template ConvertUnit<wxCommandEvent>, this);
-//    LengthField<P>::unit->name->Bind(wxEVT_KEY_UP, &LengthField<P>::template OnEditUnit<wxKeyEvent>, this);
+    LengthField<P>::unit->name->Bind(wxEVT_KEY_UP, &StaticLengthField<P>::template ConvertUnit<wxKeyEvent>, this);
     
     //add value to sizer_h, which has been initialized by the constructor of the parent class LengthField
     LengthField<P>::sizer_h->Add(value, 0, wxALIGN_CENTER | wxALL, (wxGetApp().rectangle_display.GetSize().GetWidth()) * (length_border_over_length_screen.value));

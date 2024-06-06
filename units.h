9fc7f7e46b93ffe2005325ff57bbb1709b132b13
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-06 10:53:03 +0200

    Defined some reverse unit-conversion constants

--- units.h
+++ units.h
@@ -3,16 +3,24 @@
  
     [a] = a_to_b * [b]
  
+    We habe [b] = (1/a_to_b) * [a] == b_to_a * [a], thus
+ 
+    b_to_a = 1/a_to_b .
+ 
     Example: [a] = [nm], [b] = [km],  [nm] = nm_to_km * [km] , where [nm] and [km] denote the units of nautical miles and kilometers, respectively
  
  */
 
 #define nm_to_km 1.852
+#define km_to_nm (1.0/nm_to_km)
 //[ft] = 0.3048 [m]
 //[nm] = nm_to_km [km] = nm_to_km 1000 [m] = nm_to_km 1000 / 0.3048 [ft]
 #define nm_to_ft (nm_to_km * (1e3) / 0.3048)
+#define ft_to_nm (1.0/nm_to_ft)
 //[nm] = nm_to_km [km] = nm_to_km 1000 [m]
 #define nm_to_m ((1e3)*nm_to_km)
+#define m_to_nm (1.0/nm_to_m)
 //one inch in kilometers: [inch] = inch_to_km [km]
 #define inch_to_km (25.4e-6)
+#define km_to_inch (1.0/inch_to_km)
 

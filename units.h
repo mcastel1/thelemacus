19ee7598c50316b431d1f891b93fcdeaf0ab2422
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-06 10:50:16 +0200

    Update units.h

--- units.h
+++ units.h
@@ -1,13 +1,13 @@
-//
-//  units.h
-//  minimal_cocoa
-//
-//  Created by Michele on 06/06/2024.
-//
+/*
+ the following constants are unit-coversion constants: given a unit of measure [a] and a unit of measure [b], the constant a_to_b is defined by
+ 
+    [a] = a_to_b * [b]
+ 
+    Example: [a] = [nm], [b] = [km],  [nm] = nm_to_km * [km] , where [nm] and [km] denote the units of nautical miles and kilometers, respectively
+ 
+ */
 
-//one nautical mile in kilometers: [nm] = nm_to_km * [km] where [nm] and [km] denote the units of nautical miles and kilometers, respectively, and `nm_to_km` the constant below.
 #define nm_to_km 1.852
-//one nautical mile in feet: [nm] = nm_to_ft [ft]
 //[ft] = 0.3048 [m]
 //[nm] = nm_to_km [km] = nm_to_km 1000 [m] = nm_to_km 1000 / 0.3048 [ft]
 #define nm_to_ft (nm_to_km * (1e3) / 0.3048)

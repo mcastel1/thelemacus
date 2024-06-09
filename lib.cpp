5cb0ba5342cabd958ea054216fe52c65a6fe8408
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-09 16:26:24 +0200

    Wrote  CheckSpeedUnit<P>::operator(

--- lib.cpp
+++ lib.cpp
@@ -16205,7 +16205,8 @@ template<class P> CheckLengthUnit<P>::CheckLengthUnit(DynamicLengthField<P>* p_i
 
 }
 
-//checks the unit in the GUI field in LengthField
+
+//check the unit in the GUI field in LengthField
 template<class P> template <class T> void CheckLengthUnit<P>::operator()(T& event) {
 
     P* f = (p->parent);
@@ -16322,6 +16323,54 @@ template<class P> template <class T> void CheckSpeedValue<P>::operator()(T& even
 template<class P> CheckSpeedUnit<P>::CheckSpeedUnit(SpeedField<P>* p_in) {
 
     p = p_in;
+
+}
+
+
+//check the unit in the GUI field in SpeedField
+template<class P> template <class T> void CheckSpeedUnit<P>::operator()(T& event) {
+
+    P* f = (p->parent);
+
+    //I proceed only if the progam is not is indling mode
+    if (!(f->idling)) {
+
+        unsigned int i;
+        bool check;
+
+        
+        p->unit->MultipleItemField<P, SpeedUnit, CheckSpeedUnit<P> >::CheckInCatalog(&check, &i);
+
+        if (check || (((p->unit->name->GetForegroundColour()) != (wxGetApp().error_color)) && (String(((p->unit->name->GetValue()).ToStdString())) == String("")))) {
+
+            //if check is true (false) -> set unit->ok to true (false)
+            (p->unit->ok) = check;
+            //the background color is set to white, because in this case there is no erroneous value in unit
+            p->unit->name->SetForegroundColour(wxGetApp().foreground_color);
+            p->unit->name->SetFont(wxGetApp().default_font);
+
+
+        }else {
+
+            stringstream temp;
+
+            temp.str("");
+            temp << "Available units are: ";
+            for (i = 0; i < SpeedUnit_types.size(); i++) {
+                temp << (SpeedUnit_types[i]).value << ((i < SpeedUnit_types.size() - 1) ? ", " : ".");
+            }
+
+            f->print_error_message->SetAndCall((p->unit->name), String("Unit not found in list!"), String(temp.str().c_str()), (wxGetApp().path_file_error_icon));
+
+            (p->unit->ok) = false;
+
+        }
+
+        f->AllOk();
+
+    }
+
+    event.Skip(true);
 
 }
 

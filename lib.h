ac8dff6ca8bf8902618f82aa847233b5a46b98b6
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-11 18:10:36 +0200

    created limbfield.h and filled it

--- lib.h
+++ lib.h
@@ -2296,35 +2296,7 @@ public:
 
 
 #include "bodyfield.h"
-
-
-template<class P> class LimbField{
-    
-public:
-    //the parent frame to which this object is attached
-    P* parent_frame;
-    
-    //this is the wxComboBox with the name of the limbs
-    wxCheckListBox* name;
-    wxArrayInt checked_items;
-    wxArrayString limbs;
-    //this points to a Limn object, which contains the data written in the GUI field of this
-    Limb* limb;
-    wxBoxSizer *sizer_h, *sizer_v;
-    bool ok;
-    CheckLimb<P>* check;
-    OnChangeSelectionInLimbField<P>* change_selection;
-    
-    LimbField(wxPanel*, Limb*);
-    void set(void);
-    void Enable(bool);
-    template<class T> void get(T&);
-    template<class T> void InsertIn(T*);
-    bool is_ok(void);
-    template<class E> void OnEdit(E&);
-    template <typename EventTag, typename Method, typename Object> void Bind(EventTag, Method, Object);
-    
-};
+#include "limbfield.h"
 
 
 //class for graphical object: a field to enter an angle, composed of a box for the sign, a box for the degrees, a degree text symbol, another box for minutes and a minute text symbol

91ce00b3dd88b4e0bac0c4abb03b6870fd8ef0bb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-11 23:29:08 +0200

    created projection.h and projection.cpp

---
+++ include/projection.h
@@ -0,0 +1,13 @@
+//
+//  projection.h
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#ifndef projection_h
+#define projection_h
+
+
+
+#endif
---
+++ src/projection.cpp
@@ -0,0 +1,10 @@
+//
+//  bodyfield.cpp
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#include "projection.h"
+
+

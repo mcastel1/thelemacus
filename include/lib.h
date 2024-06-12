1a3cd0b141e81245b8c63879a9eebd8d7a88300f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-12 18:02:04 +0200

    Update lib.h

--- include/lib.h
+++ include/lib.h
@@ -302,36 +302,36 @@ public:
 };
 
 
-//a non-GUI object containing a Length
-class Length{
-    
-public:
-    //the value of the Length
-    double value;
-    //the unit of measure of the Length
-    LengthUnit unit;
-    
-    Length();
-    Length(double);
-    Length(double, const LengthUnit&);
-    Length(Chrono, Speed);
-    
-    void set(String, double, String);
-    void set(double);
-    void set(double, const LengthUnit&);
-    string to_string(const LengthUnit&, unsigned int);
-    string to_string(unsigned int);
-    void print(String, String, ostream&);
-    void convert_to(const LengthUnit&);
-    Length convert(const LengthUnit&);
-    template<class S> void read_from_stream(String, S*, bool, String);
-    void read_from_file_to(String, String, String, String);
-    bool check(String, String);
-    bool operator > (const Length&), operator >= (const Length&), operator <= (const Length&), operator > (const double&), operator >= (const double&), operator <= (const double&), operator < (const Length&), operator < (const double&), operator == (const Length&), operator != (const Length&);
-    Length operator + (const Length&), operator - (const Length&),  operator * (const double&), operator / (const double&);
-    void operator += (const Length&), operator -= (const Length&), operator *= (const double&), operator /= (const double&);
-    
-};
+////a non-GUI object containing a Length
+//class Length{
+//    
+//public:
+//    //the value of the Length
+//    double value;
+//    //the unit of measure of the Length
+//    LengthUnit unit;
+//    
+//    Length();
+//    Length(double);
+//    Length(double, const LengthUnit&);
+//    Length(Chrono, Speed);
+//    
+//    void set(String, double, String);
+//    void set(double);
+//    void set(double, const LengthUnit&);
+//    string to_string(const LengthUnit&, unsigned int);
+//    string to_string(unsigned int);
+//    void print(String, String, ostream&);
+//    void convert_to(const LengthUnit&);
+//    Length convert(const LengthUnit&);
+//    template<class S> void read_from_stream(String, S*, bool, String);
+//    void read_from_file_to(String, String, String, String);
+//    bool check(String, String);
+//    bool operator > (const Length&), operator >= (const Length&), operator <= (const Length&), operator > (const double&), operator >= (const double&), operator <= (const double&), operator < (const Length&), operator < (const double&), operator == (const Length&), operator != (const Length&);
+//    Length operator + (const Length&), operator - (const Length&),  operator * (const double&), operator / (const double&);
+//    void operator += (const Length&), operator -= (const Length&), operator *= (const double&), operator /= (const double&);
+//    
+//};
 
 
 class Speed{

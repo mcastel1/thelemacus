4aee3d1027d5c5fa5d64f8b69679416ab0f04aea
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-12 14:50:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-/*
+
  
  
- - compile on APPLE:
- clear; clear; g++ main.cpp src/angle_field.cpp -o main.o `wx-config --cxxflags --libs` -lgsl -lcblas -I/usr/local/include/gsl/ -L/usr/local/bin  -I/Applications/boost_1_83_0 -L/Applications/boost_1_83_0/stage/lib/ -lboost_filesystem -lboost_system   -Wall -Wno-c++11-extensions --std=c++17  -O3 -rpath /Applications/boost_1_83_0/stage/lib -I/Users/michele/Documents/sight_reduction_program/include 
+ //- compile on APPLE:
+ //clear; clear; g++ main.cpp src/*.cpp -o main.o `wx-config --cxxflags --libs` -lgsl -lcblas -I/usr/local/include/gsl/ -L/usr/local/bin  -I/Applications/boost_1_83_0 -L/Applications/boost_1_83_0/stage/lib/ -lboost_filesystem -lboost_system   -Wall -Wno-c++11-extensions --std=c++17  -O3 -rpath /Applications/boost_1_83_0/stage/lib -I/Users/michele/Documents/sight_reduction_program/include
  
- To run on WIN32, the resource file is resource_file_windows.rc
+ //To run on WIN32, the resource file is resource_file_windows.rc
  
- */
+ 
 
 
 

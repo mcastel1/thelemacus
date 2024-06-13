37e4b2c00e58d0637f3324b7b45ea848302ba810
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-13 21:11:33 +0200

    Update lib.h

--- include/lib.h
+++ include/lib.h
@@ -125,33 +125,6 @@ template<class F> class ChartTransportHandler;
 
 
 
-
-//this is a wxFrame designed to show a message to the GUI user. FF_OK is the type of the functor class which will be called when the button ok is pressed. This type is variable, so it has been 'templated'
-template<class FF_OK> class MessageFrame: public wxFrame{
-    
-public:
-    
-    wxPanel *panel;
-    wxBoxSizer *frame_sizer, *sizer_v;
-    wxGridSizer* sizer_grid;
-    wxButton* button_ok;
-    StaticBitmap* image;
-    //    wxBitmap* m_bitmap;
-    //pointer to the class containing the functor which will be called when the button ok is pressed
-    FF_OK* f_ok;
-    
-    MessageFrame(wxWindow*, FF_OK*, const wxString&, const wxString&, String, const wxPoint&, const wxSize&, String);
-    //initialize the functor to close this MessageFrame when button_ok will be pressed
-    CloseFrame<MessageFrame>* close_frame;
-    void KeyDown(wxKeyEvent&);
-    //    void OnPaint(wxPaintEvent&);
-    //    void OnPressOk(wxCommandEvent&);
-    
-    //    DECLARE_EVENT_TABLE();
-    
-};
-
-
 //this is a wxFrame designed to ask a  yes/no question to the GUI user. F_A is the type of the functor class which will be called when the button yes is pressed. This type is variables, so it has been 'templated'. Same for F_B. If the user presses enter (esc), f_a  (f_b) are called
 template<typename F_A, typename F_B, typename F_ABORT> class QuestionFrame: public wxFrame{
     

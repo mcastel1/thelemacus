25bad90ee5a4d8285389f0dad8be242104615b0b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-13 20:56:26 +0200

    date

---
+++ include/date.h
@@ -0,0 +1,29 @@
+//
+//  date.h
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#ifndef date_h
+#define date_h
+
+
+class Date{
+    
+public:
+    unsigned int Y, M, D;
+    bool Y_is_leap_year;
+    vector<unsigned int> days_per_month;
+    
+    void print(String, String, ostream&);
+    void enter(String, String);
+    template<class S> bool read_from_stream(String, S*, bool, String);
+    string to_string(void);
+    void check_leap_year(void);
+    void set_current(void);
+    
+};
+
+
+#endif
--- include/lib.h
+++ include/lib.h
@@ -202,21 +202,6 @@ public:
 
 
 
-class Date{
-    
-public:
-    unsigned int Y, M, D;
-    bool Y_is_leap_year;
-    vector<unsigned int> days_per_month;
-    
-    void print(String, String, ostream&);
-    void enter(String, String);
-    template<class S> bool read_from_stream(String, S*, bool, String);
-    string to_string(void);
-    void check_leap_year(void);
-    void set_current(void);
-    
-};
 
 
 class Time{

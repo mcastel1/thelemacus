8161f80d0efa1ac049252c93eacbe276aa685487
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-13 20:45:57 +0200

    answer

---
+++ include/answer.h
@@ -0,0 +1,35 @@
+//
+//  answer.h
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#ifndef answer_h
+#define answer_h
+
+
+class Answer{
+    
+public:
+    char value;
+    
+    Answer();
+    Answer(char, String);
+    void enter(String, String);
+    bool set(String, char, String);
+    //the print function takes an arbitrary ostream for output, which can be equal to cout if we want to print otuput to terminal, or to a file ofstream if we want to print the output to a file
+    void print(String, String, ostream&);
+    template<class S> void read_from_stream(String, S*, bool, String);
+    void read_from_file_to(String, String, String, String);
+    bool operator==(const Answer&), operator !=(const Answer&);
+    
+};
+
+
+
+
+
+
+
+#endif
--- include/lib.h
+++ include/lib.h
@@ -197,28 +197,6 @@ public:
 
 
 
-class Answer{
-    
-public:
-    char value;
-    
-    Answer();
-    Answer(char, String);
-    void enter(String, String);
-    bool set(String, char, String);
-    //the print function takes an arbitrary ostream for output, which can be equal to cout if we want to print otuput to terminal, or to a file ofstream if we want to print the output to a file
-    void print(String, String, ostream&);
-    template<class S> void read_from_stream(String, S*, bool, String);
-    void read_from_file_to(String, String, String, String);
-    bool operator==(const Answer&), operator !=(const Answer&);
-    
-};
-
-
-
-
-
-
 
 //a derived class from String, which stores the type of a Projection ("Mercator", "3D", ...)
 class Projection: public String{

b225dd5b327c874f6b4b5a1e5b7324ae4e2ef551
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-13 21:00:28 +0200

    atmo

---
+++ include/atmosphere.h
@@ -0,0 +1,29 @@
+//
+//  atmosphere.h
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#ifndef atmosphere_h
+#define atmosphere_h
+
+
+class Atmosphere{
+    
+public:
+    
+    Length earth_radius;
+    unsigned int n_layers;
+    double A, B, P_dry_0, alpha, beta, gamma, T0;
+    vector<Length> /*heights delimiting the level of the US 1976 atmosphere model, see https://en.wikipedia.org/wiki/U.S._Standard_Atmosphere, expressed in units of LengthUnit_types[0] */h;
+    vector<double> /*dT/dz, ecpressed in units of K / nm*/lambda, t;
+    void set(void);
+    double T(Length), n(Length), dTdz(Length), dndz(Length);
+    
+};
+
+
+
+
+#endif
---
+++ src/atmosphere.cpp
@@ -0,0 +1,183 @@
+//
+//  atmosphere.cpp
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#include "atmosphere.h"
+
+
+
+
+
+double Atmosphere::T(Length z) {
+
+    double x = 0.0;
+    //cout << "z = " << (z.value) << "\n";
+
+    if (z <= h[n_layers]) {
+
+        unsigned int i;
+        bool check = true;
+
+        for (i = 0, check = true; (i < n_layers) && check; i++) {
+            if ((z >= h[i]) && (z < h[i + 1])) {
+                x = t[i] + lambda[i] * ((z - h[i]).convert(LengthUnit_types[0]).value);
+                check = false;
+            }
+        }
+        //cout << "i = " << i << "\n";
+
+    }
+    else {
+
+        cout << RED << "Value of z is not valid!\n" << RESET;
+        x = -1.0;
+
+    }
+
+    return x;
+
+
+}
+
+double Atmosphere::dTdz(Length z) {
+
+    double x = 0.0;
+    //cout << "z = " << (z.value) << "\n";
+
+    if (z <= h[n_layers]) {
+
+        unsigned int i;
+        bool check = true;
+
+        for (i = 0, check = true; (i < n_layers) && check; i++) {
+            if ((z >= h[i]) && (z < h[i + 1])) {
+                x = lambda[i];
+                check = false;
+            }
+        }
+
+    }
+    else {
+
+        cout << RED << "Value of z is not valid!\n" << RESET;
+        x = -1.0;
+
+    }
+
+    return x;
+
+
+}
+
+
+double Atmosphere::n(Length z) {
+
+    double x = 0.0;
+
+    if (z <= h[n_layers]) {
+
+        unsigned int i;
+        bool check = true;
+
+        for (i = 0, x = 0.0, check = true; (i < n_layers) && check; i++) {
+            if ((z >= h[i]) && (z < h[i + 1])) {
+                if (lambda[i] != 0.0) {
+                    x -= B / lambda[i] * log((t[i] + lambda[i] * ((z - h[i]).convert(LengthUnit_types[0]).value)) / t[i]);
+                }
+                else {
+                    x -= B * ((z - h[i]).convert(LengthUnit_types[0]).value) / t[i];
+                }
+                check = false;
+            }
+            else {
+                if (lambda[i] != 0.0) {
+                    x -= B / lambda[i] * log((t[i] + lambda[i] * ((h[i + 1] - h[i]).convert(LengthUnit_types[0]).value)) / t[i]);
+                }
+                else {
+                    x -= B * ((h[i + 1] - h[i]).convert(LengthUnit_types[0]).value) / t[i];
+                }
+            }
+        }
+
+        /*
+         int_0^z dz/(t_n+lambda_n*(z-h_n)) = log()
+         */
+
+    }
+    else {
+
+        cout << RED << "Value of z is not valid!\n" << RESET;
+        x = -1.0;
+
+    }
+
+    return (A * P_dry_0 / T(z) * exp(x) / (1e6) + 1.0);
+
+
+}
+
+double Atmosphere::dndz(Length z) {
+
+    return (-1.0 / T(z) * dTdz(z) * (n(z) - 1.0) - (n(z) - 1.0) * B / T(z));
+
+}
+
+
+
+void Atmosphere::set(void) {
+
+    unsigned int i;
+    double x;
+    bool check;
+
+    //cout << "Atmosphere model: US 1976.\n";
+
+    n_layers = 7;
+    A = 0.7933516713545163;
+    B = 34.16 * nm_to_km;
+    P_dry_0 = 101325.0;
+    alpha = -6.5 * nm_to_km;
+    beta = 2.8 * nm_to_km;
+    gamma = -2.8 * nm_to_km;
+    T0 = 288.15;
+    earth_radius.value = Re;
+
+    h.resize(n_layers + 1);
+    lambda.resize(n_layers);
+    t.resize(n_layers);
+
+    
+    h[0] = 0.0;
+    h[1] = 11.0 * km_to_nm;
+    h[2] = 20.0 * km_to_nm;
+    h[3] = 32.0 * km_to_nm;
+    h[4] = 47.0 * km_to_nm;
+    h[5] = 51.0 * km_to_nm;
+    h[6] = 71.0 * km_to_nm;
+    h[7] = 84.8520 * km_to_nm;
+
+    lambda[0] = -6.5 * nm_to_km;
+    lambda[1] = 0.0 * nm_to_km;
+    lambda[2] = 1.0 * nm_to_km;
+    lambda[3] = 2.8 * nm_to_km;
+    lambda[4] = 0.0 * nm_to_km;
+    lambda[5] = -2.8 * nm_to_km;
+    lambda[6] = -2.0 * nm_to_km;
+
+
+    for (i = 0, x = T0, check = true; (i < n_layers) && check; i++) {
+        t[i] = x;
+        x += lambda[i] * ((h[i + 1] - h[i]).convert(LengthUnit_types[0]).value);
+    }
+
+
+    /* for(int i=0; i<4+1; i++){ */
+    /*   cout << "\t\t" << i << " " << h[i] << "\n"; */
+    /* } */
+
+
+}
+

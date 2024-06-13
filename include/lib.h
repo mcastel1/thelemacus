674f2bc570c40205b490cef360a165179887e796
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-13 21:05:34 +0200

    list control

--- include/lib.h
+++ include/lib.h
@@ -599,28 +599,6 @@ public:
 };
 
 
-//my own derived class of wxListCtrl, which contains objects of type S
-template<class S> class ListControl : public wxListCtrl{
-    
-public:
-    
-    //a vector containing pointers to the buttons of *this (modify button, etc ...) which may be disabled if some event occurs
-    vector<wxButton*> disableable_buttons;
-    //header_width[i] = width of the header of the i-th column
-    vector<int> header_width;
-    
-    ListControl(wxWindow*, vector<wxButton*>, const wxPoint&, const wxSize&, long);
-    
-    void SetColumns(vector<wxString>);
-    void PushBackColumn(wxString);
-    void DeselectAll(void);
-    void set(vector<S>, bool);
-    void EnableButtons(bool);
-    void Resize(vector<S>);
-    void GetSelectedItems(vector<long>*);
-    
-};
-
 
 
 //if an item is selected/deselected in caller, enable/disable the disableable buttons in caller

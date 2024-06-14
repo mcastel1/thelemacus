798d593fdd2c059a8abb58a0b3a7393197b10448
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-14 19:02:37 +0200

    datefield

---
+++ include/date_field.h
@@ -0,0 +1,50 @@
+//
+//  date_field.h
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#ifndef date_field_h
+#define date_field_h
+
+#include <iostream>
+
+using namespace std;
+
+
+template<class P> class DateField{
+    
+public:
+    //the parent frame to which this object is attached
+    P* parent_frame;
+    wxArrayString days, months;
+    //year, month and day boxes
+    wxTextCtrl *year;
+    wxComboBox *month, *day;
+    //texts
+    StaticText* text_hyphen_1, *text_hyphen_2;
+    wxBoxSizer *sizer_h, *sizer_v;
+    //this points to a Date object, which contains the date written in the GUI fields of this
+    Date* date;
+    //year_ok = true if the year is formatted properly and set to the same value as date->Y, and similarly for the other variables
+    bool year_ok, month_ok, day_ok;
+    CheckDate<P>* check;
+    
+    DateField(wxPanel*, Date*);
+    void set(Date);
+    void SetBackgroundColor(Color);
+    template<class T> void get(T&);
+    template<class T> void InsertIn(T*);
+    bool is_ok(void);
+    template<class E> void OnEditYear(E&);
+    template<class E> void OnEditMonth(E&);
+    template<class E> void OnEditDay(E&);
+    template <typename EventTag, typename Method, typename Object> void Bind(EventTag, Method, Object);
+    
+    
+};
+
+
+
+#endif
--- src/lib.cpp
+++ src/lib.cpp
@@ -729,20 +729,7 @@ template<class P> template <class T> void CheckDate<P>::operator()(T& event) {
 
 }
 
-//this functor writes the values written inthe whole GUI date field (year, month and day) in the respective non-GUI object date->D, date->M, date->D
-template<class P> template <class T> void DateField<P>::get(T& event) {
 
-    if (year_ok && (year->IsEnabled()) && month_ok && (month->IsEnabled()) && day_ok && (month->IsEnabled())) {
-
-        (date->Y) = (unsigned int)wxAtoi(year->GetValue());
-        (date->M) = (unsigned int)wxAtoi(month->GetValue());
-        (date->D) = (unsigned int)wxAtoi(day->GetValue());
-
-    }
-
-    event.Skip(true);
-
-}
 
 template<class P> CheckLengthValue<P>::CheckLengthValue(DynamicLengthField<P>* p_in) {
 
@@ -2135,14 +2122,6 @@ template<class P, class T> template <typename EventTag, typename Method, typenam
 
 
 
-//set color as the background color in all fields of *this
-template<class P> void DateField<P>::SetBackgroundColor(Color color) {
-
-    year->SetBackgroundColour(color);
-    month->SetBackgroundColour(color);
-    day->SetBackgroundColour(color);
-
-}
 
 
 //sets the value in the GUI object value equal to the value in the non-GUI String object string
@@ -2222,194 +2201,6 @@ template<class P> StringField<P>::StringField(wxPanel* panel_of_parent, String* 
 
 
 
-//constructor of a DateField object, based on the parent frame frame
-template<class P> DateField<P>::DateField(wxPanel* panel_of_parent, Date* p) {
-
-    unsigned int i;
-    parent_frame = ((P*)(panel_of_parent->GetParent()));
-    date = p;
-
-
-    //initialize check and its objects
-    check = new CheckDate<P>(this);
-    //    (check.p) = this;
-    //    (check->check_year->p) = this;
-    //    ((check->check_year->tabulate_days).p) = this;
-    //    (check->check_month->p) = this;
-    //    ((check->check_month->tabulate_days).p) = this;
-    //    (check->check_day->p) = this;
-
-    for (months.Clear(), months.Add(wxT("")), i = 0; i < 12; i++) {
-        months.Add(wxString::Format(wxT("%i"), i + 1));
-    }
-
-    year = new wxTextCtrl(parent_frame->panel, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
-    //SetColor(year);
-    year->SetInitialSize(year->GetSizeFromTextSize(year->GetTextExtent(wxS("0000"))));
-    year->SetValue(wxString(""));
-    year_ok = false;
-    year->Bind(wxEVT_KILL_FOCUS, *(check->check_year));
-    //as text is changed year by the user with the keyboard, call OnEditYear
-    year->Bind(wxEVT_KEY_UP, &DateField::OnEditYear<wxKeyEvent>, this);
-
-
-    text_hyphen_1 = new StaticText((parent_frame->panel), wxT("-"), wxDefaultPosition, wxDefaultSize, 0);
-    month = new wxComboBox(parent_frame->panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, months, wxCB_DROPDOWN | wxTE_PROCESS_ENTER);
-    //SetColor(month);
-    AdjustWidth(month);
-    month->SetValue(wxString(""));
-    month_ok = false;
-    month->Bind(wxEVT_KILL_FOCUS, *(check->check_month));
-    //as text is changed in name from the user, i.e., with either a keyboard button or a selection in the listbox, call OnEdit
-    month->Bind(wxEVT_COMBOBOX, &DateField::OnEditMonth<wxCommandEvent>, this);
-    month->Bind(wxEVT_KEY_UP, &DateField::OnEditMonth<wxKeyEvent>, this);
-
-    text_hyphen_2 = new StaticText((parent_frame->panel), wxT("-"), wxDefaultPosition, wxDefaultSize, 0);
-
-    day = new wxComboBox(parent_frame->panel, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, days, wxCB_DROPDOWN | wxTE_PROCESS_ENTER);
-    //SetColor(day);
-    //I create a temporary days list to set the size of the wxComboBox day with AdjustWidth, and then destroy this temporary days list
-    for (days.Clear(), i = 0; i < 31; i++) {
-        days.Add(wxString::Format(wxT("%i"), i + 1));
-    }
-    day->Set(days);
-    AdjustWidth(day);
-    days.Clear();
-    day->SetValue(wxString(""));
-    day_ok = false;
-    day->Bind(wxEVT_KILL_FOCUS, *(check->check_day));
-    //as text is changed in day from the user, i.e., with either a keyboard button or a selection in the listbox, call OnEditDay
-    day->Bind(wxEVT_COMBOBOX, &DateField::OnEditDay<wxCommandEvent>, this);
-    day->Bind(wxEVT_KEY_UP, &DateField::OnEditDay<wxKeyEvent>, this);
-
-
-
-
-
-    sizer_h = new wxBoxSizer(wxHORIZONTAL);
-    sizer_v = new wxBoxSizer(wxVERTICAL);
-
-    sizer_v->Add(sizer_h, 0, wxALIGN_LEFT);
-    sizer_h->Add(year, 0, wxALIGN_CENTER);
-    sizer_h->Add(text_hyphen_1, wxALIGN_CENTER);
-    sizer_h->Add(month, 0, wxALIGN_CENTER);
-    sizer_h->Add(text_hyphen_2, wxALIGN_CENTER);
-    sizer_h->Add(day, 0, wxALIGN_CENTER);
-
-}
-
-
-
-
-
-
-
-
-template<class P> bool DateField<P>::is_ok(void) {
-
-    return(year_ok && month_ok && day_ok);
-
-}
-
-
-//this function is called every time a keyboard button is lifted in this->year: it checks whether the text entered so far in year is valid and runs AllOk
-template<class P> template<class E> void DateField<P>::OnEditYear(E& event) {
-
-    bool success;
-
-    success = check_unsigned_int((year->GetValue()).ToStdString(), NULL, false, 0, 0);
-
-    if (success) {
-
-        year->SetForegroundColour(wxGetApp().foreground_color);
-        year->SetFont(wxGetApp().default_font);
-
-    }
-
-    //year_ok is true/false is the text enteres is valid/invalid
-    year_ok = success;
-
-    (*(((this->check)->check_month)->tabulate_days))(event);
-
-    //tries to enable button_reduce
-    parent_frame->AllOk();
-
-    event.Skip(true);
-
-}
-
-//this function is called every time a keyboard button is lifted in this->month: it checks whether the text entered so far in month is valid and runs AllOk
-template<class P> template<class E> void DateField<P>::OnEditMonth(E& event) {
-
-    bool success;
-
-    success = check_unsigned_int((month->GetValue()).ToStdString(), NULL, true, 1, 12 + 1);
-
-    if (success) {
-
-        month->SetForegroundColour(wxGetApp().foreground_color);
-        month->SetFont(wxGetApp().default_font);
-
-    }
-
-    //month_ok is true/false is the text enteres is valid/invalid
-    month_ok = success;
-
-    (*(((this->check)->check_month)->tabulate_days))(event);
-
-    //tries to enable button_reduce
-    parent_frame->AllOk();
-
-    event.Skip(true);
-
-}
-
-//this function is called every time a keyboard button is lifted in this->day: it checks whether the text entered so far in day is valid and runs AllOk
-template<class P> template<class E> void DateField<P>::OnEditDay(E& event) {
-
-    bool success;
-
-    date->check_leap_year();
-
-    if (date->Y_is_leap_year) {
-
-        success = check_unsigned_int((day->GetValue()).ToStdString(), NULL, true, 1, days_per_month_leap[(wxAtoi(month->GetValue())) - 1] + 1);
-
-    }
-    else {
-
-        success = check_unsigned_int((day->GetValue()).ToStdString(), NULL, true, 1, days_per_month_common[(wxAtoi(month->GetValue())) - 1] + 1);
-
-    }
-
-    if (success) {
-
-        day->SetForegroundColour(wxGetApp().foreground_color);
-        day->SetFont(wxGetApp().default_font);
-
-    }
-
-    //day_ok is true/false is the text enteres is valid/invalid
-    day_ok = success;
-
-    //tries to enable button_reduce
-    parent_frame->AllOk();
-
-    event.Skip(true);
-
-}
-
-
-
-template<class P> template <typename EventTag, typename Method, typename Object> void DateField<P>::Bind(EventTag tag, Method method, Object object) {
-
-    //I bind year, month and day to method
-    year->Bind(tag, method, object);
-    month->Bind(tag, method, object);
-    day->Bind(tag, method, object);
-
-}
-
 
 
 
@@ -2421,13 +2212,6 @@ template<class P, class T> template<class R> void CheckField<P, T>::InsertIn(R* 
 }
 
 
-
-
-template<class P> template<class T> void DateField<P>::InsertIn(T* host) {
-
-    host->Add(sizer_v);
-
-}
 
 
 template<class P> template<class T> void StringField<P>::InsertIn(T* host) {

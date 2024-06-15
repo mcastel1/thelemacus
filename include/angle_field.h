b2ff281642293f101fd5e5f08da243a9bc81c0af
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-15 23:25:33 +0200

    put back format in anglefield

--- include/angle_field.h
+++ include/angle_field.h
@@ -39,6 +39,8 @@ public:
     //the parent frame to which this object is attached
     P* parent;
     wxArrayString signs, degrees;
+    //the format of the AngleField: String("") for angles  between 0 and 2*pi, String("+-") for angles with a sign between 0 and pi, String("NS") for latitudes between -pi/2 and pi/2, and String("EW") for longitudes between -pi and pi
+    String format;
     //degrees and minutes boxes
     wxComboBox*sign, * deg;
     wxTextCtrl *min;

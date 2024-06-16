32876b7a811a49f152eab409e8c69d974156c70e
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-16 18:11:11 +0200

    Update chart_frame.cpp

--- src/chart_frame.cpp
+++ src/chart_frame.cpp
@@ -1303,8 +1303,8 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
     wxPoint q;
 
 //    //transform the values i_min, i_max in a format appropriate for GetCoastLineData: normalize the minimal and maximal latitudes in such a way that they lie in the interval [-pi, pi], because this is the format which is taken by GetCoastLineData
-//    phi_min.normalize_pm_pi();
-//    phi_max.normalize_pm_pi();
+//    phi_min->normalize_pm_pi();
+//    phi_max->normalize_pm_pi();
 //
 //
     if ((lambda_min < M_PI) && (lambda_max > M_PI)) {
@@ -1329,8 +1329,8 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
 
     }
 
-    i_min = floor(K * (phi_min.normalize_pm_pi_ret().value));
-    i_max = floor(K * (phi_max.normalize_pm_pi_ret().value));
+    i_min = floor(K * (phi_min->normalize_pm_pi_ret().value));
+    i_max = floor(K * (phi_max->normalize_pm_pi_ret().value));
 
 
     if ((parent->show_coastlines) == Answer('y', String(""))) {
@@ -1339,7 +1339,7 @@ void ChartFrame::GetCoastLineData_Mercator(void) {
         unsigned long long int n_added_polygons, l, p, m, every;
         bool new_polygon;
         
-//        ( ((phi_max.normalize_pm_pi_ret() - phi_min.normalize_pm_pi_ret()).value)*((lambda_max->normalize_pm_pi_ret() - lambda_min->normalize_pm_pi_ret()).value) ) / ( (ceil_max_lat - floor_min_lat)*2*M_PI );
+//        ( ((phi_max->normalize_pm_pi_ret() - phi_min->normalize_pm_pi_ret()).value)*((lambda_max->normalize_pm_pi_ret() - lambda_min->normalize_pm_pi_ret()).value) ) / ( (ceil_max_lat - floor_min_lat)*2*M_PI );
         
         /*
          the number of points plotted is [number of coastline data points in lambda_min ... phi_max] / every = n_points_plot_coastline_Mercator ->

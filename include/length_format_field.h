62d09e0e1b58ff72acb3d3b205cbeefd3f51f923
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-16 21:44:08 +0200

    Update length_format_field.h

--- include/length_format_field.h
+++ include/length_format_field.h
@@ -16,6 +16,8 @@
 
 using namespace std;
 
+template<class P> class CheckLengthFormat;
+
 
 //this class defines a dropdown menu (wxComboBox) that lets the user choose in what format to express lengths, i.e., simply as a DynamicLengthField or as a ChronoField + a SpeedField (l = t * v). P is the type of parent in which *this is inserted
 template<class P> class LengthFormatField: public MultipleItemField<P, LengthFormat, CheckLengthFormat<P> >{

a9955743d98917a5e04237bafad5411956ddd4d7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-17 19:12:13 +0200

    show question frame

--- include/lib.h
+++ include/lib.h
@@ -664,31 +664,6 @@ public:
 
 
 
-//this functor pops out a question window with title tile, quesiton question, and answers answer_y, answer_n, resulting from the wxControl control. The type of the frame from which the error message is printed is T, and it is variable so as to make this class adaptable. If the user answers yes/no to the question, f_yes/f_no are called
-template<class T, typename F_YES, typename F_NO, typename F_ABORT> class ShowQuestionFrame{
-    
-public:
-    
-    T* f;
-    wxControl* control;
-    String title, message, question, answer_y, answer_n;
-    F_YES* f_yes;
-    F_NO* f_no;
-    F_ABORT* f_abort;
-    QuestionFrame<F_YES, F_NO, F_ABORT>* question_frame;
-    bool /*these are true/false if the yes/no button are enabled/disabled, respectively*/ enable_button_a, enable_button_b, /*if this is true, then pressing esc will be equivalent to pressing button b, if this is false, pressing esc will call *f_abort*/ bind_esc_to_button_b;
-    
-    ShowQuestionFrame(T*, F_YES*, F_NO*, F_ABORT*);
-    
-    void SetAndCall(wxControl*, String, String, String, String);
-    void SetAndCall(wxControl*, String, String, String, String, bool, bool, bool);
-    void EnableDisableButtons(void);
-    void operator()(void);
-    
-    
-};
-
-
 
 
 

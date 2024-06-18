7294c204254b4b7afe73c3c27bebbc0d265c15dd
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 14:11:04 +0200

    ...

--- include/lib.h
+++ include/lib.h
@@ -429,20 +429,3 @@ public:
     
 };
 
-
-//a hanlder to transport a non-GUI object of type NON_GUI (NON_GUI may be equal to Position, Route, ...)  with an animation. When the transport is over, this may exectued a functor of type F. If nothing is supposed to be exectued at the end of the transport, set F = void
-template <class NON_GUI, class F> class GraphicalFeatureTransportHandler: public MotionHandler<F>{
-    
-public:
-    
-    NON_GUI* transported_object;
-    //the type of the transported_object that is being transported (String("position") or String("route"), ...)
-    String type_of_transported_object;
-    
-    GraphicalFeatureTransportHandler(ListFrame*, NON_GUI*, const String&, const Route&, F*);
-    void operator()(void);
-
-    void OnTimer(wxTimerEvent&);
-    
-};
-

b2f77db6fd4e04b8f1e8529a81bf933912c07ab9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 09:41:32 +0200

    check year

--- include/lib.h
+++ include/lib.h
@@ -154,17 +154,6 @@ template<class F> class ChartTransportHandler;
 
 
 
-template<class P> class CheckYear{
-    
-public:
-    
-    DateField<P>* p;
-    TabulateDays<P>* tabulate_days;
-    
-    CheckYear(DateField<P>*);
-    template<class T> void operator()(T&);
-    
-};
 
 template<class P> class CheckMonth{
     
--- src/lib.cpp
+++ src/lib.cpp
@@ -588,13 +588,6 @@ template<class P> template <class T> void CheckSpeed<P>::operator()(T& event) {
 
 
 
-template<class P> CheckYear<P>::CheckYear(DateField<P>* p_in) {
-
-    p = p_in;
-    tabulate_days = new TabulateDays<P>(p);
-
-}
-
 template<class P> CheckMonth<P>::CheckMonth(DateField<P>* p_in) {
 
     p = p_in;
@@ -608,53 +601,6 @@ template<class P> CheckDay<P>::CheckDay(DateField<P>* p_in) {
 
 }
 
-
-template<class P> template<class T> void CheckYear<P>::operator()(T& event) {
-
-    SightFrame* f = (p->parent_frame);
-
-    //I proceed only if the progam is not is indling mode
-    if (!(f->idling)) {
-
-        bool check;
-
-        check = check_unsigned_int((p->year->GetValue()).ToStdString(), NULL, false, 0, 0);
-
-        if (check || (((p->year->GetForegroundColour()) != (wxGetApp().error_color)) && (String(((p->year->GetValue()).ToStdString())) == String("")))) {
-            //p->year either contains a valid text, or it is empty and with a white background color, i.e., virgin -> I don't call an error message frame
-
-            //if check is true (false) -> set year_ok to true (false)
-            (p->year_ok) = check;
-            //the background color is set to white, because in this case there is no erroneous value in year
-            p->year->SetForegroundColour(wxGetApp().foreground_color);
-            p->year->SetFont(wxGetApp().default_font);
-
-            if (check && (p->month_ok)) {
-
-                (*tabulate_days)(event);
-
-            }
-
-            p->day->Enable(check && (p->month_ok));
-
-
-        }
-        else {
-
-            f->print_error_message->SetAndCall((p->year), String("Entered value is not valid!"), String("Year must be an unsigned integer"), (wxGetApp().path_file_error_icon));
-
-            (p->year_ok) = false;
-            p->day->Enable(false);
-
-        }
-
-        f->AllOk();
-
-    }
-
-    event.Skip(true);
-
-}
 
 template<class P> template<class T> void CheckMonth<P>::operator()(T& event) {
 

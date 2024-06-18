ada29e2a44c877d9067c36d400de2fafaacf96eb
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 14:17:50 +0200

    class delete route

--- src/lib.cpp
+++ src/lib.cpp
@@ -294,51 +294,6 @@ template<class P> void FunctionOnPressOk<P>::operator()(wxCommandEvent& event) {
 }
 
 
-//constructor of the struct, which initializes the Answer remove_related_sight. If remove_related_sight.value = 'y', then DeleteRoute::operator() will delete both the route and the related sight. If remove_related_sight.value = 'n', then it will remove the route only.
-DeleteRoute::DeleteRoute(ListFrame* f_in, Answer remove_related_sight_in) {
-
-    f = f_in;
-    remove_related_sight = remove_related_sight_in;
-
-}
-
-void DeleteRoute::operator()(wxCommandEvent& event) {
-
-    int i_related_sight;
-
-    i_related_sight = ((((f->data)->route_list)[i_route_to_remove]).related_sight).value;
-
-    //remove the sight related to the route which I am about to remove from the GUI object listcontrol_sights
-    if ((i_related_sight != -1) && (remove_related_sight == Answer('y', String("")))) {
-
-        //        (f->listcontrol_sights)->DeleteItem(i_related_sight);
-
-        //given that after one item is deleted in listcontrol_sights, now no item is selected in listcontrol_sights, I call:
-        (*(f->on_change_selection_in_listcontrol_sights))(event);
-
-
-    }
-
-    //I remove the route and the related sight from both the non-GUI object data
-    (f->data)->remove_route(((unsigned int)i_route_to_remove), remove_related_sight, String(""));
-
-    (f->listcontrol_sights)->set((f->data)->sight_list, false);
-    (f->listcontrol_routes)->set((f->data)->route_list, false);
-
-    //given that I called set in listcontrol_routes, now no item is selected in listcontrol_routes, I call:
-    (*(f->on_change_selection_in_listcontrol_routes))(event);
-    f->Resize();
-    //given that a Route has been removed, I re-draw everything
-    f->PreRenderAll();
-
-    event.Skip(true);
-
-}
-
-
-
-
-
 template<class P> template <class T> void SetStringFieldToCurrentTime<P>::operator()(T& event) {
 
     //if the label is empty, I replace it with the local time and date

87cadca3a42d3efe0adca3294d3c65efaafacc77
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 10:23:57 +0200

    Update check_field.h

--- include/check_field.h
+++ include/check_field.h
@@ -18,6 +18,8 @@
 
 using namespace std;
 
+template<class P, class T> class CheckCheck;
+
 
 //this is a GUI field contaning a binary checkbox, which is either checked or unchecked
 template<class P, class T> class CheckField{

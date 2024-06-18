515c6d5d6fee6fbfc46164a618b9ad0d56b26a3d
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 14:39:21 +0200

    Update route_frame.cpp

--- src/route_frame.cpp
+++ src/route_frame.cpp
@@ -7,6 +7,7 @@
 
 #include "route_frame.h"
 #include "speed_unit_field.h"
+#include "string_field.h"
 
 
 //create a new RouteFrame. If for_transport = true/false, it enables the fields related to the start position of the Route and disables the circle of equal altitude type

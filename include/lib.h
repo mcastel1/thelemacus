96359b55540f767b6a063e75154352ceb28a12c8
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-18 14:06:20 +0200

    chart transport handler

--- include/lib.h
+++ include/lib.h
@@ -446,24 +446,3 @@ public:
     
 };
 
-
-//a hanlder to move the chart of a given ChartFrame with an animation
-template<class F> class ChartTransportHandler: public MotionHandler<F>{
-    
-public:
-    
-    //a poitner to the ChartFrame whose chart is being moved
-    ChartFrame* chart_frame;
-    
-    PositionProjection /*the size (in Mercator projection x,y) of the projection in the mercator projection: this is stored and used during the transport in such a way that the size of the projection stays the same through the transport*/projection_size, /*same as projection_size, but for the initial/final step of the animation */projection_size_start, projection_size_end;
-    //the NS, SW and center Position of the chart, used for the Mercator projections
-    Position p_NE, p_SW, p_center;;
-    //for the 3d projectionb: the aperture angles of circle_observer  at the beginning and at the end of the transport, respectively
-    Angle omega_start, omega_end;
-    Double zoom_factor;
-
-    ChartTransportHandler(ChartFrame*, const Route&, const Double&, F*);
-    void operator()(void);
-    void OnTimer(wxTimerEvent&);
-    
-};

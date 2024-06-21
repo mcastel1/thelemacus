ca55607b64c26768718646d8cb82eef8c5f5c16b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-21 17:11:25 +0200

    Moved all inline functions in generic.cpp to generic.h

--- src/generic.cpp
+++ src/generic.cpp
@@ -9,54 +9,6 @@
 
 
 
-inline double cot(double x){
-    
-    return 1.0/tan(x);
-    
-}
-
-inline double csc(double x){
-    
-    return 1.0/sin(x);
-    
-}
-
-//returns alpha = arctan(y/x), with alpha lying in the same quadrant as the vector x, y
-inline double atan(double x, double y){
-    
-    if(x != 0.0){
-        
-        if(x > 0.0){
-            
-            return atan(y/x);
-            
-        }else{
-            
-            return(atan(y/x)+M_PI);
-        }
-        
-    }else{
-        
-        if(y > 0.0){
-            
-            return(M_PI/2.0);
-            
-        }else{
-            
-            return(3.0*M_PI/2.0);
-            
-        }
-        
-    }
-    
-}
-
-//the floor of the exponential of x
-inline int floor_exp(double x){
-    
-    return(floor(exp(x)));
-    
-}
 
 //given a wxListCtrl* list_control, if * list_control has at least one item, it finds on what element of *list_control the mouse is currently on and writes the id of this element in i. Otherwise, it writes wxNOT_FOUND in *i
 void MousePositionOnListControl(wxListCtrl* list_control, int* i){
@@ -355,54 +307,6 @@ void enter_double(double* x, bool check_interval, double min, double sup, String
     
 }
 
-//round the floating point number x with precision `precision`
-inline double round_with_precision(double x, unsigned int precision)
-{
-    return round(x * gsl_pow_int(10.0, precision)) / gsl_pow_int(10.0, precision);
-}
-
-inline double sin(const Angle& x) {
-
-    return sin(x.value);
-
-}
-
-inline double asin(const Double& x) {
-
-    return asin(x.value);
-
-}
-
-
-inline double tan(const Angle& x) {
-
-    return tan(x.value);
-
-}
-
-inline double csc(const Angle& x) {
-
-    return csc(x.value);
-
-}
-
-inline double sec(const Angle& x) {
-
-    return (1.0 / cos(x));
-
-}
-
-inline double cot(const Angle& x) {
-
-    return cot(x.value);
-
-}
-
-inline double acos(const Double& x) {
-
-    return acos(x.value);
-
-}
 
 //find the  position in v of element with value x and return the position. If no element is found, return v.size(). Note that this function is different from address_position_in_vector
 template<class T> unsigned int position_in_vector(T x, const vector<T>& v){
@@ -506,32 +410,6 @@ wxSize get_size(const String& s, wxDC* dc) {
 }
 
 
-//put the angle x in the interval [-pi, pi), it does not alter *this and returns the result. This is equivalent to Angle::normalize_pm_pi_ret
-inline Angle normalize_pm_pi_ret(const Angle& x){
-        
-    Angle temp;
-
-    temp = x;
-
-    return(temp.normalize_pm_pi_ret());
-
-}
-
-
-//normalize a and b between -pi and pi, and return  the algebraic mean between a.value and b.value
-inline double mean_pm_pi(Angle& a, Angle& b){
-    
-    return(((a.normalize_pm_pi_ret().value) + (b.normalize_pm_pi_ret().value))/2.0);
-    
-}
-
-
-//return the mean between a.value and b.value
-inline double mean_value(Angle& a, Angle& b){
-    
-    return( ((a.value) + (b.value))/2.0 );
-    
-}
 
 
 string to_string(const Position& p, unsigned int precision) {
@@ -546,60 +424,6 @@ string to_string(const Position& p, unsigned int precision) {
 
 
 
-
-//compute the cross product between the three-dimensional vectors a and b, and write the result into c, which is cleared and re-allocated. It returs true if the size of both a and b is 3, and false otherwise. If false is returned, r is not touched.
-inline bool my_cross(const gsl_vector* a, const gsl_vector* b, gsl_vector** r) {
-
-    if (((a->size) == 3) && ((b->size) == 3)) {
-
-        if (((*r) != NULL) && (((*r)->size) != 0)) {
-
-            gsl_vector_free(*r);
-
-        }
-
-        (*r) = gsl_vector_alloc(3);
-
-
-
-        gsl_vector_set(*r, 0, gsl_vector_get(a, 1) * gsl_vector_get(b, 2) - gsl_vector_get(a, 2) * gsl_vector_get(b, 1));
-        gsl_vector_set(*r, 1, gsl_vector_get(a, 2) * gsl_vector_get(b, 0) - gsl_vector_get(a, 0) * gsl_vector_get(b, 2));
-        gsl_vector_set(*r, 2, gsl_vector_get(a, 0) * gsl_vector_get(b, 1) - gsl_vector_get(a, 1) * gsl_vector_get(b, 0));
-
-
-        return true;
-
-    }
-    else {
-
-        return false;
-
-    }
-
-
-}
-
-
-//checks whether s is present into wxArrayString, and writes true/false into check if its present/absent. If i!=NULL: if it is present, it writes the position of s in v in *i, if it is not present, i is not touched
-inline void find_and_replace_case_insensitive(wxComboBox* control, wxArrayString v, bool* check, unsigned int* i) {
-
-    unsigned int j;
-
-    for ((*check) = false, j = 0; (j < v.size()) && (!(*check)); j++) {
-
-        if ((bool)(boost::iequals((control->GetValue()).ToStdString(), (v[j]).ToStdString()))) {
-
-            (*check) = true;
-            //I write in control the proper text value (i.e. with the correct upper/lower case, as taken from the list v)
-            control->SetValue(v[j]);
-
-        }
-
-    }
-
-    if (i != NULL) { (*i) = j - 1; }
-
-}
 
 //sets fore/background colors to a generic object of type T
 template<class T> void SetColor(T* object) {
@@ -734,25 +558,6 @@ template<class S> void read_list_from_stream(String name, S* input_stream, bool 
 
     } while (pos_b != string::npos);
 
-}
-
-
-
-
-
-
-//this function returns the longitude value (expressed in degrees, positive towards W) of the inverse spherical Mercator projection from the rectangular x value
-inline double lambda_mercator(double x){
-    
-    return (-x*rad_to_deg - 360.0*floor((x-(-M_PI))/(2.0*M_PI)));
-    
-}
-
-//this function returns the latitude value (expressed in degrees) of the inverse spherical Mercator projection from the rectangular y value
-inline double phi_mercator(double y){
-    
-    return(rad_to_deg*atan(sinh(y)));
-    
 }
 
 

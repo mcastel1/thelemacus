79229da034e62d865c420bbaece008c3083d402a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-06-28 16:50:11 +0200

    Update multiple_item_field.cpp

--- src/multiple_item_field.cpp
+++ src/multiple_item_field.cpp
@@ -237,8 +237,8 @@ template<class P, class NON_GUI, class CHECK> template<class E> void MultipleIte
 }
 
 template void MultipleItemField<RouteFrame, LengthFormat, CheckLengthFormat<RouteFrame>>::Check<wxFocusEvent>(wxFocusEvent&);
-
 template void MultipleItemField<RouteFrame, RouteType, CheckRouteType<RouteFrame>>::Check<wxFocusEvent>(wxFocusEvent&);
+template void MultipleItemField<RouteFrame, RouteType, CheckRouteType<RouteFrame>>::Check<wxCommandEvent>(wxCommandEvent&);
 
 
 //update the GUI dropdown menu of MultipleItemField according to MultipleItemField<P, NON_GUI>::recent_items in such a way that the recent items appear on top

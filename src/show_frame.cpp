060093214e324ab7734c6b35044a86a863220f67
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-07-01 19:27:17 +0200

    Update show_frame.cpp

--- src/show_frame.cpp
+++ src/show_frame.cpp
@@ -15,6 +15,8 @@ template<class F> ShowFrame<F>::ShowFrame(F* frame_in) {
 
 }
 
+template ShowFrame<ListFrame>;
+
 
 //show a frame of type F
 template<class F> template <class T> void ShowFrame<F>::operator()(T& event) {
@@ -23,5 +25,6 @@ template<class F> template <class T> void ShowFrame<F>::operator()(T& event) {
 
     event.Skip(true);
 
-
 }
+
+template void ShowFrame<ListFrame>::operator()<wxCommandEvent>(wxCommandEvent&);

e258934de8e82f791bd2e5e1e8dc59fe5ae30b37
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-07-03 23:27:20 +0200

    Fixed bug that the radii were set with their memory addresses

--- src/body.cpp
+++ src/body.cpp
@@ -85,10 +85,11 @@ template<class S> bool Body::read_from_stream(String name_in, S* input_stream, [
 //set the *content* (not the memory adresses) of *this equal to the content of input
 void Body::set(const Body& input){
     
-    name->set(*(input.name));
-    type->set(*(input.type));
+    name->set((*(input.name)));
+    type->set((*(input.type)));
         
-    radius = (input.radius);
+//    radius = (input.radius);
+    radius->set((*(input.radius)));
     
     (RA->value) = (input.RA->value);
     (d->value) = (input.d->value);  

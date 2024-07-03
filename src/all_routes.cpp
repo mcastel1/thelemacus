dd9cadb46f721f51b7e8dac2df40063fda477ce2
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-07-03 23:12:42 +0200

    Update all_routes.cpp

--- src/all_routes.cpp
+++ src/all_routes.cpp
@@ -21,10 +21,10 @@ void AllRoutes::operator()(wxCommandEvent& event) {
     unsigned int j;
 
     //there need to be at list two routes of type ((Route_types[2]).value) to compute crossings. Here I include all routes of type ((Route_types[2]).value) whhich are related to a Sight into crossing_route_list by writing their index into crossing_route_list
-    for (((f->data)->crossing_route_list).clear(), j = 0; j < (f->data->route_list).size(); j++) {
+    for ((f->data->crossing_route_list).clear(), j = 0; j < (f->data->route_list).size(); j++) {
 
         if (((((f->data->route_list))[j]).type.value) == ((Route_types[2]).value)) {
-            ((f->data)->crossing_route_list).push_back((((f->data->route_list))[j]));
+            (f->data->crossing_route_list).push_back((((f->data->route_list))[j]));
         }
 
     }

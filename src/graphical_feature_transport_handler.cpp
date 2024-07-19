70c05d511bd2ff4d05bebc84802f7be909cf796b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-07-19 14:14:05 +0200

    ... done

--- src/graphical_feature_transport_handler.cpp
+++ src/graphical_feature_transport_handler.cpp
@@ -197,7 +197,6 @@ template<class NON_GUI, class F> void GraphicalFeatureTransportHandler<NON_GUI, 
                 //do the whole transport rather than combining many little transports, to avoid rounding errors
                 //                (*(((Route*)transported_object)->reference_position)) = (*(MotionHandler<F>::start));
                 ((Route*)transported_object)->reference_position->set((*(MotionHandler<F>::start)));
-                
                 ((Route*)transported_object)->reference_position->transport_to((MotionHandler<F>::transporting_route), String(""));
 
 

d0aa476a274b2e7317c590344c9af77a48b38996
mcastel1 <michele.castellana@gmail.com>
2024-08-05 10:13:22 +0200

    created bodyname

---
+++ src/body_name.cpp
@@ -0,0 +1,24 @@
+//
+//  body_name.cpp
+//  thelemacus
+//
+//  Created by Michele on 11/06/2024.
+//
+
+#include "body_name.h"
+
+
+BodyName::BodyName(void) : String() {}
+
+
+BodyName::BodyName(const String& input) : String(input.value) {}
+
+
+//return true if *this is one element in BodyName_types (i.e. *this is a valid Length unit) and false otherwise)
+//inline 
+bool BodyName::check(void){
+    
+    return(position_in_list(BodyName_types) != (BodyName_types.size())) ;
+    
+}
+

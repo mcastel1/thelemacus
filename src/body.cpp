447323dedfcb180d12182dad38ef3b0bebdb2a54
mcastel1 <michele.castellana@gmail.com>
2024-08-05 11:47:17 +0200

    Update body.cpp

--- src/body.cpp
+++ src/body.cpp
@@ -58,7 +58,8 @@ template<class S> bool Body::read_from_stream(String name_in, S* input_stream, [
         line.clear();
         getline(*input_stream, line);
         pos = line.find(" = ");
-        (*name) = line.substr(pos + 3, line.size() - (pos + 3));
+        
+        name->set(line.substr(pos + 3, line.size() - (pos + 3)));
         cout << new_prefix.value << "Name = " << name->value << "\n";
 
 

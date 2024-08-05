8e896a8c6dcf0166e7cc67b7a55fb588e3e1e221
mcastel1 <michele.castellana@gmail.com>
2024-08-05 10:27:15 +0200

    Catalog::position_in_list

--- src/body_name.cpp
+++ src/body_name.cpp
@@ -17,7 +17,7 @@ BodyName::BodyName(const String& input) : String(input.value) {}
 //return true if *this is one element in BodyName_types (i.e. *this is a valid Body name) and false otherwise)
 bool BodyName::check(void){
     
-    return(position_in_list(BodyName_types) != (BodyName_types.size())) ;
+//    return(position_in_list(BodyName_types) != (BodyName_types.size())) ;
     
 }
 

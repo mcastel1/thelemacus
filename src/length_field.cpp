448ae1f31cfc36e28bbd31b0004c725302709a50
mcastel1 <michele.castellana@gmail.com>
2024-08-11 22:34:24 +0200

    Update length_field.cpp

--- src/length_field.cpp
+++ src/length_field.cpp
@@ -55,53 +55,53 @@ template class LengthField<RouteFrame>;
 template class LengthField<SightFrame>;
 
 
-//this function is called every time a keyboard button is lifted in this->unit: it checks whether the text entered so far in unit is valid and runs AllOk
-template<class P> template<class E> void LengthField<P>::OnEditUnit(E& event) {
-
-    bool success;
-
-    //I check whether the name in the GUI field unit matches one of the unit names in units
-    find_and_replace_case_insensitive(unit->name, unit->catalog, &success, NULL);
-
-
-    if (success) {
-
-        //because the text in value is valid, I set the background color of unit to white
-        unit->name->SetForegroundColour(wxGetApp().foreground_color);
-        unit->name->SetFont(wxGetApp().default_font);
-
-    }
-
-    (unit->ok) = success;
-
-    event.Skip(true);
-
-}
-
-template void LengthField<RouteFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
-template void LengthField<RouteFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
-template void LengthField<SightFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
-template void LengthField<SightFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
-template void LengthField<ChartFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
-template void LengthField<ChartFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
+////this function is called every time a keyboard button is lifted in this->unit: it checks whether the text entered so far in unit is valid and runs AllOk
+//template<class P> template<class E> void LengthField<P>::OnEditUnit(E& event) {
+//
+//    bool success;
+//
+//    //I check whether the name in the GUI field unit matches one of the unit names in units
+//    find_and_replace_case_insensitive(unit->name, unit->catalog, &success, NULL);
+//
+//
+//    if (success) {
+//
+//        //because the text in value is valid, I set the background color of unit to white
+//        unit->name->SetForegroundColour(wxGetApp().foreground_color);
+//        unit->name->SetFont(wxGetApp().default_font);
+//
+//    }
+//
+//    (unit->ok) = success;
+//
+//    event.Skip(true);
+//
+//}
+//
+//template void LengthField<RouteFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
+//template void LengthField<RouteFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
+//template void LengthField<SightFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
+//template void LengthField<SightFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
+//template void LengthField<ChartFrame>::OnEditUnit<wxKeyEvent>(wxKeyEvent&);
+//template void LengthField<ChartFrame>::OnEditUnit<wxCommandEvent>(wxCommandEvent&);
 
 
 //insert *this in *host
-template<class P> template<class T> void LengthField<P>::InsertIn(T* host) {
-
-    host->Add(LengthField<P>::sizer_v);
-
-}
-
-template void LengthField<RouteFrame>::InsertIn<wxFlexGridSizer>(wxFlexGridSizer*);
-template  void LengthField<SightFrame>::InsertIn<wxFlexGridSizer>(wxFlexGridSizer*);
-
-
-//same asLengthField<P>::InsertIn(T* host) but with flags to be provided
-template<class P> template<class T> void LengthField<P>::InsertIn(T* host, wxSizerFlags& flag) {
-
-    host->Add(sizer_v, flag);
-
-}
-
-template void LengthField<ChartFrame>::InsertIn<wxBoxSizer>(wxBoxSizer*, wxSizerFlags&);
+//template<class P> template<class T> void LengthField<P>::InsertIn(T* host) {
+//
+//    host->Add(LengthField<P>::sizer_v);
+//
+//}
+//
+//template void LengthField<RouteFrame>::InsertIn<wxFlexGridSizer>(wxFlexGridSizer*);
+//template  void LengthField<SightFrame>::InsertIn<wxFlexGridSizer>(wxFlexGridSizer*);
+//
+//
+////same asLengthField<P>::InsertIn(T* host) but with flags to be provided
+//template<class P> template<class T> void LengthField<P>::InsertIn(T* host, wxSizerFlags& flag) {
+//
+//    host->Add(sizer_v, flag);
+//
+//}
+//
+//template void LengthField<ChartFrame>::InsertIn<wxBoxSizer>(wxBoxSizer*, wxSizerFlags&);

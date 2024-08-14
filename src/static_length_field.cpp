e5d29b8bfec1e9930ef126261b6b195f7c704f64
mcastel1 <michele.castellana@gmail.com>
2024-08-14 16:17:26 +0200

    Update static_length_field.cpp

--- src/static_length_field.cpp
+++ src/static_length_field.cpp
@@ -74,7 +74,7 @@ template<class P> void StaticLengthField<P>::set_value_keep_unit(void) {
     
     Length temp;
     
-    temp = (LengthField<P>::object->convert(LengthUnit(LengthField<P>::unit->name->GetValue().ToStdString())));
+    temp = (length->convert(LengthUnit(unit->name->GetValue().ToStdString())));
     
     set(temp);
 

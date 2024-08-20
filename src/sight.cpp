e7e6b98a8d2172a5d7bc2cc7d0101cc9c1b1b353
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-08-20 10:21:13 +0200

    Update sight.cpp

--- src/sight.cpp
+++ src/sight.cpp
@@ -177,7 +177,7 @@ void Sight::update_ListControl(long i, wxListCtrl* listcontrol) {
     listcontrol->SetItem(i, j++, wxString(H_s.to_string(String(""), (display_precision.value), true)));
 
     //set index error
-    listcontrol->SetItem(i, j++, wxString((index_error).to_string(String(""), (display_precision.value), true)));
+    listcontrol->SetItem(i, j++, wxString(index_error.to_string(String(""), (display_precision.value), true)));
 
     //set height of eye column
     if (artificial_horizon.value == 'n') {

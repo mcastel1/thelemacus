8707ffc0fa50ffa83766f13f19c353eeefe6b424
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-08-20 11:35:33 +0200

    Update length_format_field.h

--- include/length_format_field.h
+++ include/length_format_field.h
@@ -23,27 +23,12 @@ template<class P> class CheckLengthFormat;
 template<class P> class LengthFormatField: public MultipleItemField<P, LengthFormat, CheckLengthFormat<P> >{
     
 public:
-    
-//    P* parent;
-//    wxBoxSizer *sizer_h, *sizer_v;
-//    wxArrayString  /*this is equal to a standard list of the available formats for a length*/items, /*same as length_formats, but it is fixed and never wrote to: it is a fixed catalog*/catalog;
-    //this is the wxComboBox with the name of the length formats
-//    wxComboBox* name;
-    //this points to a LengthFormat object, which contains the date written in the GUI field of this
-//    vector<int> recent_items;
-//    bool ok;
-//    LengthFormat* length_format;
-//    CheckLengthFormat<P>* check;
 
     LengthFormatField(wxPanel*, LengthFormat*, vector<int>*);
-//    void FillInRecentItems(void);
-//    void set(void);
-//    template<class T> void InsertIn(T*);
+
     template<class E> void OnEdit(E&);
-//    void Enable(bool);
-//    bool is_ok(void);
-//    template <typename EventTag, typename Method, typename Object> void Bind(EventTag, Method, Object);
     
+   
 };
 
 

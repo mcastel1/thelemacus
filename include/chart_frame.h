ac869187ec206799719af5f819cb884ef29f09d5
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-08-23 20:30:00 +0200

    Update chart_frame.h

--- include/chart_frame.h
+++ include/chart_frame.h
@@ -53,8 +53,8 @@ public:
     PrintMessage<ChartFrame, UnsetIdling<ChartFrame> >* print_error_message;
     /*the latitude/longitude setting the boundaries of the plotted area on earth*/Angle *phi_min, *phi_max, /*it is not necessarily true that lambda_min < lambda_max: lambda_min(max) correspond to the left(right) edge of the plot area*/*lambda_min, *lambda_max;
     //a vector of the points of the coastlines in DrawPanel coordinates: these are points of polygons concatenated in the linear arrays coastline_polygons_now/ before. The i-th polygon is stored n coastline_polygons_now/before[polygon_position_now/before[i]], coastline_polygons_now[polygon_position_now/before[i]+1], ..., coastline_polygons_now[polygon_position_now/before[i+1]-1]
-    vector<wxPoint> coastline_polygons_now/*, coastline_polygons_before*/;
-    vector<unsigned long long int> polygon_position_now/*, polygon_position_before*/;
+    vector<wxPoint> coastline_polygons_now/*, coastline_polygons_before*/, grid_points;
+    vector<unsigned long long int> polygon_position_now/*, polygon_position_before*/, grid_positions;
 
     //idling = true means that the user is interacting with a temporary dialog window, thus all the handlers of wxFOCUS_EVENT do not make sense when idling = true and they will be disabled until idling is set back to false
     bool idling, /*this is true if the user is currently scrolling*/mouse_scrolling, /*this is true if the chart is being dragged, and thus the size of *this must not change across multiple Draw(s), and false otherwise*/ dragging_chart;

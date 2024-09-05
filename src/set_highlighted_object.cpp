672de613147afe68dfa73be19c6d836b7f98ad6c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-05 17:39:24 +0200

    Update set_highlighted_object.cpp

--- src/set_highlighted_object.cpp
+++ src/set_highlighted_object.cpp
@@ -11,12 +11,16 @@
     #include "wx/wx.h"
 #endif
 
+#include "list_frame.h"
+
 
 //initialize this by setting parent equal to parent_in, higlighted_object_before equal to higlighted_object_before_in and higlighted_object_now equal to higlighted_object_now_in
 template<class P> SetHighlightedObject<P>::SetHighlightedObject(P* parent_in, int* highlighted_object_before_in, int* highlighted_object_now_in):  parent(parent_in), higlighted_object_before(highlighted_object_before_in), highlighted_object_now(highlighted_object_now_in) {
 
 
 }
+
+template class SetHighlightedObject<ListFrame>;
 
 
 template<class P> void SetHighlightedObject<P>::set_value(const int& i){

8031e27061474e177fb11e16bacf9a23ac39f950
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-09 14:47:20 +0200

    Update chart_frame.cpp

--- src/chart_frame.cpp
+++ src/chart_frame.cpp
@@ -209,8 +209,6 @@ ChartFrame::ChartFrame(ListFrame* parent_in, Projection projection_in, const wxS
     sizer_h->Add(sizer_slider, 0, wxALIGN_TOP | wxALL | wxEXPAND, 0);
 
     sizer_v->Add(sizer_h, 0, wxALIGN_LEFT | wxALL, (wxGetApp().rectangle_display.GetSize().GetWidth()) * (length_border_over_length_screen.value));
-    //    sizer_v->Add(text_position_now, 0, wxALIGN_LEFT | wxALL, (wxGetApp().rectangle_display.GetSize().GetWidth()) * (length_border_over_length_screen.value));
-        //    sizer_v->Fit(panel);
 
 #ifdef _WIN32
     //if I am on WIN32, I set the icon from the icon set in the .rc file

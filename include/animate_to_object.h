5cf9e7c3b6cfa8eb96446676dc81c698474dc104
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-09-20 22:25:43 +0200

    Created AnimateToObject<T, F>::operator()(E& event)

--- include/animate_to_object.h
+++ include/animate_to_object.h
@@ -10,23 +10,27 @@
 
 #include <iostream>
 
+#include "list_frame.h"
+
 using namespace std;
 
 
-//this functor makes an animation which centers the chart on the object *object (which may be a Route, Position, ...) and adjust the chart zoom factor in such a way that *object is nicely visible at the end of the animation. Here f is the functor of the function that will be called at the end of the animation, and it will be entered into the constructor of ChartTransrportHandler. If no functor is to be called at the end of the animation, one may let f point to a UnsetIdling<> functor. The template parameters are : P (type of the parent of *this), T (type of the object to which animate), and F (the type of the functor to be called at the end of the animation)
-template<class P, class T, class F> class AnimateToObject{
+//this functor makes an animation which centers the chart on the object *object (which may be a Route, Position, ...) and adjust the chart zoom factor in such a way that *object is nicely visible at the end of the animation. Here f is the functor of the function that will be called at the end of the animation, and it will be entered into the constructor of ChartTransrportHandler. If no functor is to be called at the end of the animation, one may let f point to a UnsetIdling<> functor. The template parameters are : T (type of the object to which animate), and F (the type of the functor to be called at the end of the animation). The animation will be triggered by calling template<class E> void operator()(E&);
+template<class T, class F> class AnimateToObject{
     
 public:
         
     //the parent of *this
-    P* parent;
+    ListFrame* parent;
     //the object to which animate
     T* object;
     //the functor to be called at the end of the animation
     F* f;
     
-    AnimateToObject(P*, T*, F*);
+    AnimateToObject(ListFrame*, T*, F*);
     
+    template<class E> void operator()(E&);
+
 };
 
 

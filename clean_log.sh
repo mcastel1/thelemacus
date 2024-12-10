2287445fba13837eee8a5c353bd84349027a136a
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-12-10 14:01:23 +0100

    CU

--- clean_log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -n '/ld: Undefined symbols:/,$p' ~/Desktop/Build*.txt > ~/Desktop/output.dat
-wc -l ~/Desktop/output.dat
--- make_toy_app.sh
+++ make_toy_app.sh
@@ -9,14 +9,14 @@
 clear; clear;
 
 #the name of the app that you want to create
-APP_NAME='Thelemacus'
+APP_NAME='ToyThelemacus'
 APP_VERSION='1.0'
 MINIMUM_SYSTEM_VERSION='10.13'
 #the path to the icon of the app
 ICON_NAME='app_icon.icns'
 ICON_PATH='/Users/michelecastellana/Documents/thelemacus/Contents/Resources/Images/'$ICON_NAME
 #the path to the .cpp file
-INPUT_PATH='/Users/michelecastellana/Documents/thelemacus'
+INPUT_PATH='/Applications/wxWidgets-3.2.6/samples/minimal'
 OUTPUT_PATH='/Users/michelecastellana/Desktop'
 APP_LIBRARY_DIRECTORY=$OUTPUT_PATH'/'$APP_NAME'.app/Contents/Resources/Libraries'
 WXWIDGETS_ROOT_DIRECTORY='/Applications/wxWidgets-3.2.6-release'
@@ -45,7 +45,7 @@ mkdir -p $OUTPUT_PATH/$APP_NAME.app/Contents/Resources/Libraries
 
 
 #compile Thelemacus
-g++ main.cpp src/*.cpp -o $APP_NAME  `wx-config --cxxflags --libs` -lgsl -lcblas -I/usr/local/include/gsl/ -L/usr/local/bin  -I/Applications/boost_1_83_0 -L/Applications/boost_1_83_0/stage/lib/ -lboost_filesystem -lboost_system   -Wall -Wno-c++11-extensions --std=c++17  -O3 -rpath /Applications/boost_1_83_0/stage/lib -I$INPUT_PATH/include -I$INPUT_PATH/src
+g++ $INPUT_PATH/*.cpp -o $APP_NAME  `wx-config --cxxflags --libs` -lgsl -lcblas -I/usr/local/include/gsl/ -L/usr/local/bin  -I/Applications/boost_1_83_0 -L/Applications/boost_1_83_0/stage/lib/ -lboost_filesystem -lboost_system   -Wall -Wno-c++11-extensions --std=c++17  -O3 -rpath /Applications/boost_1_83_0/stage/lib -I$INPUT_PATH/include -I$INPUT_PATH/src
 
 
 #set links fo $APP_NAME
